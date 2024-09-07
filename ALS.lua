332d42f09af56693fbdf8fdcfdfa6a87d8b5165f
SenDo <champandbank1@gmail.com>
2024-09-07 15:56:47 +0700

    Update ALS.lua

--- ALS.lua
+++ ALS.lua
@@ -9,7 +9,7 @@ elseif tableFind(PlaceId, game.PlaceId) then
 end
 
 local var,err = pcall(function ()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sendo-cmd/test/main/" .. GameLoad .. ".lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sendo-cmd/GCS/main/" .. GameLoad .. ".lua"))()
 end)
 
 if var == false  then
