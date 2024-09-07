7a70c3c9ecde22b424803215bbeb2328d0836aa8
SenDo <champandbank1@gmail.com>
2024-09-07 16:07:03 +0700

    Update ALS.lua

--- ALS.lua
+++ ALS.lua
@@ -2,7 +2,9 @@ local GameLoad = nil
 local PlaceId = {
 	12886143095,18583778121,12900046592
 }
-if tableFind(PlaceId, game.PlaceId) then
+if game.PlaceId == 6299805723 then
+    GameLoad = "Banana"
+elseif tableFind(PlaceId, game.PlaceId) then
     GameLoad = "Kaitunlist"
 elseif tableFind(PlaceId, game.PlaceId) then
     GameLoad = "Reroll"
