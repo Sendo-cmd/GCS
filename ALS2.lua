341aad954d797ba3ee857cbcf1251d377408b54c
SenDo <champandbank1@gmail.com>
2024-09-08 16:07:27 +0700

    Update ALS2.lua

--- ALS2.lua
+++ ALS2.lua
@@ -1,7 +1,9 @@
 local GameLoad = nil
 local PlaceId = {
-	12886143095,18583778121,12900046592
+	12886143095, 18583778121, 12900046592
 }
+
+-- Check if game.PlaceId is in the PlaceId table
 for _, id in ipairs(PlaceId) do
     if game.PlaceId == id then
         GameLoad = "Reroll"
@@ -9,10 +11,15 @@ for _, id in ipairs(PlaceId) do
     end
 end
 
-local var,err = pcall(function ()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sendo-cmd/GCS/main/" .. GameLoad .. ".lua"))()
-end)
+-- Attempt to load the script from the URL only if GameLoad is set
+if GameLoad then
+    local var, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Sendo-cmd/GCS/main/" .. GameLoad .. ".lua"))()
+    end)
 
-if var == false  then
-    print("Error : " .. err)
+    if not var then
+        print("Error: " .. err)
+    end
+else
+    print("Error: GameLoad is nil, PlaceId not found.")
 end
