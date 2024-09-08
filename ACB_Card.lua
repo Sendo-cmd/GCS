99ff51ede375c2d420240fc588e7b64588080c45
SenDo <champandbank1@gmail.com>
2024-09-08 22:52:55 +0700

    Update ACB_Card.lua

--- ACB_Card.lua
+++ ACB_Card.lua
@@ -7,6 +7,7 @@ ACBCardNotification = {
         ["Krv_kai43"] = "https://discord.com/api/webhooks/1281841266216276039/_nRzrWKIVEvuqRS9pp0MMHxKCcQCxCaStOlGu0_uhOHMdNUHAiqfc1YrsV7cuaN66rl8",
         ["Puggtopro"] = "https://discord.com/api/webhooks/1281991050835263520/IAgXcVnYeuRc0_DK_PpXZZbz3VinaVqnQ6ZLUCwAat2x79-0U1jyRVk45BG9slx-qcno",
         ["mbhl67679"] = "https://discord.com/api/webhooks/1282049202985373756/BJ1exJVnT7ve45-WxIhVW9ZtPwtCMAJ0pklZH2P2Vq4VPWhZRoZSnJwVfEcLXW-ED4d7",
+        ["Nelida5865"] = "https://discord.com/api/webhooks/1282367730988613726/ZgvFAVVTjJ1UlQDN_pUfzTzKmZmZZqPY3DylZ6pgOKdHuQTWDXcb3Q_yrEWX7wzOObFM",
     }
 }
 
