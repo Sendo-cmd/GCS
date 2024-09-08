c57865babf3ac28e92332170898dff155e5c4bc7
SenDo <champandbank1@gmail.com>
2024-09-08 15:55:31 +0700

    Update ALS2.lua

--- ALS2.lua
+++ ALS2.lua
@@ -2,9 +2,7 @@ local GameLoad = nil
 local PlaceId = {
 	12886143095,18583778121,12900046592
 }
-if game.PlaceId == 6299805723 then
-    GameLoad = "Banana"    
-elseif table.find(PlaceId,game.PlaceId) then
+if able.find(PlaceId,game.PlaceId) then
     GameLoad = "Reroll"    
 end
 
