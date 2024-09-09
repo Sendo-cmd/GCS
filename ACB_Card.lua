2c831154366c7615184627d25db0b6600172a197
SenDo <champandbank1@gmail.com>
2024-09-09 19:40:12 +0700

    Update ACB_Card.lua

--- ACB_Card.lua
+++ ACB_Card.lua
@@ -78,46 +78,51 @@ game:GetService("ReplicatedStorage").Remotes.ClientEffects.OnClientEvent:Connect
         local CardName = args[2].CardName
         local Rarity = args[2].CardRarity
 
-        if CardChance < Setting["Chance"] then return end;
+        if CardChance >= Setting["Chance"] then
 
-        local Stats = HelpfulModule.CalculateStats(CardName, Rarity)
-        local Info = CardInfo[CardName]
+            local Stats = HelpfulModule.CalculateStats(CardName, Rarity)
+            local Info = CardInfo[CardName]
 
-        local data = {
-            ["username"] = "Gaming Champion Shop",
-            ["avatar_url"] = "https://media.discordapp.net/attachments/774011709358080021/1267843051997630535/hpfk2.1.png?ex=66dc5b5a&is=66db09da&hm=b1ccca5bd7c854a11e5ad76e4c4506bc0be8b215d718df30a399fe5e0e2495b0&=&format=webp&quality=lossless&width=490&height=488",
-            ["content"] = "",
-            ["embeds"] = {
-                {
-                    ["title"] = "Anime Card Battle",
-                    ["description"] = `Username: ||{plr.Name}||\n**Rolled Card**\nCard: {CardName} [{Rarity}]\n(1 in {HelpfulModule.FormatNumber(CardChance)})\nHealth: {Stats.Health}\nDamage: {Stats.Damage}\n` .. 'Description:```yaml\n' .. Info.Description .. '```',
-                    ["thumbnail"] = {
-                        url = GetAvatarThumbnail(plr.UserId)
-                    },
-                    ["color"] = RGBtoInt(unpack(RarityColor[Rarity])),
-                    ["image"] = {
-                        ["url"] = GetCardImage(CardName),
-                    },
-                    ["author"] = {
-                        ["name"] = " Gaming Champion Shop ",
-                        ["icon_url"] = "https://media.discordapp.net/attachments/774011709358080021/1267843051997630535/hpfk2.1.png?ex=66dbb29a&is=66da611a&hm=6085a6699c2f332eca878e6fc64167c3c1065708c0ed32401e675a3eee11f9a1&=&format=webp&quality=lossless&width=490&height=488"
-                    },
-                    ["footer"] = {
-                        ["text"] = "Gaming Champion Shop ",
-                         ["icon_url"] = "https://media.discordapp.net/attachments/774011709358080021/1267843051997630535/hpfk2.1.png?ex=66dbb29a&is=66da611a&hm=6085a6699c2f332eca878e6fc64167c3c1065708c0ed32401e675a3eee11f9a1&=&format=webp&quality=lossless&width=490&height=488"
-                    },     
-                    ["timestamp"] = DateTime.now():ToIsoDate()
-                }
-            },
-        }
-
-        local response = request({
-            ["Url"] = Setting["Webhook"][plr.Name],
-            ["Method"] = "POST",
-            ["Body"] = game.HttpService:JSONEncode(data),
-            ["Headers"] = {
-                ["content-type"] = "application/json"
+            local data = {
+                ["username"] = "Gaming Champion Shop",
+                ["avatar_url"] = "https://media.discordapp.net/attachments/774011709358080021/1267843051997630535/hpfk2.1.png?ex=66dc5b5a&is=66db09da&hm=b1ccca5bd7c854a11e5ad76e4c4506bc0be8b215d718df30a399fe5e0e2495b0&=&format=webp&quality=lossless&width=490&height=488",
+                ["content"] = "",
+                ["embeds"] = {
+                    {
+                        ["title"] = "Anime Card Battle",
+                        ["description"] = `Username: ||{plr.Name}||\n**Rolled Card**\nCard: {CardName} [{Rarity}]\n(1 in {HelpfulModule.FormatNumber(CardChance)})\nHealth: {Stats.Health}\nDamage: {Stats.Damage}\n` .. 'Description:```yaml\n' .. Info.Description .. '```',
+                        ["thumbnail"] = {
+                            url = GetAvatarThumbnail(plr.UserId)
+                        },
+                        ["color"] = RGBtoInt(unpack(RarityColor[Rarity])),
+                        ["image"] = {
+                            ["url"] = GetCardImage(CardName),
+                        },
+                        ["author"] = {
+                            ["name"] = " Gaming Champion Shop ",
+                            ["icon_url"] = "https://media.discordapp.net/attachments/774011709358080021/1267843051997630535/hpfk2.1.png?ex=66dbb29a&is=66da611a&hm=6085a6699c2f332eca878e6fc64167c3c1065708c0ed32401e675a3eee11f9a1&=&format=webp&quality=lossless&width=490&height=488"
+                        },
+                        ["footer"] = {
+                            ["text"] = "Gaming Champion Shop ",
+                            ["icon_url"] = "https://media.discordapp.net/attachments/774011709358080021/1267843051997630535/hpfk2.1.png?ex=66dbb29a&is=66da611a&hm=6085a6699c2f332eca878e6fc64167c3c1065708c0ed32401e675a3eee11f9a1&=&format=webp&quality=lossless&width=490&height=488"
+                        },     
+                        ["timestamp"] = DateTime.now():ToIsoDate()
+                    }
+                },
             }
-        })
+
+            local response = request({
+                ["Url"] = Setting["Webhook"][plr.Name],
+                ["Method"] = "POST",
+                ["Body"] = game.HttpService:JSONEncode(data),
+                ["Headers"] = {
+                    ["content-type"] = "application/json"
+                }
+            })
+
+            print("Sent Webhook")
+        end
     end
 end)
+
+print("Loaded ACB")
