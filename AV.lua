d55779878f74a25a4ec84dc2f10274f314621543
SenDo <champandbank1@gmail.com>
2024-09-09 17:26:13 +0700

    Update AV.lua

--- AV.lua
+++ AV.lua
@@ -1,2 +1,29 @@
-_G.key = ""
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1282640420446404608/1282640862320263291/chap1.json?ex=66e017e3&is=66dec663&hm=e46ac10427e9a6d9e066418a698c66fdef55201b9aa89c6208a910c91a20ca48&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Stage Joiner"] = {
+		["Act"] = "Act1",
+		["Stage"] = "Planet Namak",
+		["Auto Join"] = true
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Legend Stage Joiner"] = {
+		["Stage"] = "Sand Village",
+		["Act"] = "Act3"
+	},
+	["Auto Skip Wave"] = true,
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "chap1",
+		["Play"] = true,
+		["Ignore Macro Timing"] = true
+	}
+}
+getgenv().Key = ""
 loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
