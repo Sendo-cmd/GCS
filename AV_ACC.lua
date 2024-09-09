c78d77d61b7dd669e2299dbdd7dbe25f01d92dc8
SenDo <champandbank1@gmail.com>
2024-09-10 02:38:56 +0700

    Update AV_ACC.lua

--- AV_ACC.lua
+++ AV_ACC.lua
@@ -1,11 +1,11 @@
 getgenv().Config = {
     Accounts = {
         { ID = 5176905367, script = 3 },
-        { ID = 73027822, script = 1 },
+        { ID = 73027822, script = 4 },
         { ID = 4291006444, script = 2 },
         { ID = 1686715557, script = 1 },
         { ID = 2864627356, script = 1 },
-        { ID = 7321781708, script = 4 },
+        { ID = 7321781708, script = 0 },
         { ID = 0, script = 4 }
     },
     Scripts = { 
@@ -13,7 +13,7 @@ getgenv().Config = {
         ["Script 1"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_GEM.lua",
         ["Script 2"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_GEM2.lua",
         ["Script 3"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_IGRIS.lua",
-        ["Script 4"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/",
+        ["Script 4"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_NARUTO.lua",
         ["Script 5"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/",
     }
 }
