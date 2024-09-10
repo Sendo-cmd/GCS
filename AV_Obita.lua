fa865fb69fe68ab8422c3dd0071274c5aeae916d
SenDo <champandbank1@gmail.com>
2024-09-10 15:17:55 +0700

    Update AV_Obita.lua

--- AV_Obita.lua
+++ AV_Obita.lua
@@ -1,3 +1,5 @@
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1282792802576502784/NarutoEvo.json?ex=66e0a565&is=66df53e5&hm=9b385aea370f372587e29c32be9d235ab5d2bf1a0f3240aa29d5572531a72748&"
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Webhook"] = {
@@ -23,3 +25,5 @@ getgenv().Config = {
 		["Auto Replay"] = true
 	}
 }
+getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
