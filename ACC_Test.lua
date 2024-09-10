0e3a66a355d3fb565aa66ebc85f8a4df6c39900a
SenDo <champandbank1@gmail.com>
2024-09-11 00:00:37 +0700

    Create ACC_Test.lua

---
+++ ACC_Test.lua
@@ -0,0 +1,23 @@
+getgenv().Config = {
+    Accounts = {
+        { ID = 5176905367, script = 3 },
+        { ID = "FireBlackDevilZ", script = 5 },
+        { ID = 4291006444, script = 1 },
+        { ID = 1686715557, script = 1 },
+        { ID = 2864627356, script = 1 },
+        { ID = 7321781708, script = 6 },
+        { ID = 1885237561, script = 1 },
+        { ID = 938632969, script = 1 },
+        { ID = 5053824822, script = 2 },
+    },
+    Scripts = { 
+        -- Please make it a raw link 
+        ["Script 1"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_GEM.lua",
+        ["Script 2"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_GEM2.lua",
+        ["Script 3"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_IGRIS.lua",
+        ["Script 4"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_NARUTO.lua",
+        ["Script 5"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_Obita.lua",
+        ["Script 6"] = "https://raw.githubusercontent.com/Sendo-cmd/GCS/main/AV_CHAIN.lua",
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sendo-cmd/test/main/MultiExecute.lua"))()
