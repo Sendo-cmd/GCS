c26b71c0c1d9dc943ef9d2838f5df2578fdf1a9a
SenDo <champandbank1@gmail.com>
2024-09-10 16:30:52 +0700

    Update AV_ACC.lua

--- AV_ACC.lua
+++ AV_ACC.lua
@@ -6,7 +6,7 @@ getgenv().Config = {
         { ID = 1686715557, script = 1 },
         { ID = 2864627356, script = 1 },
         { ID = 7321781708, script = 0 },
-        { ID = 1885237561, script = 6 }
+        { ID = 1885237561, script = 1 }
     },
     Scripts = { 
         -- Please make it a raw link 
