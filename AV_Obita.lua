27dff35cd2b79a597c64cf120a1e423e3da0109e
SenDo <champandbank1@gmail.com>
2024-09-10 15:17:16 +0700

    Create AV_Obita.lua

---
+++ AV_Obita.lua
@@ -0,0 +1,25 @@
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Webhook"] = {
+		["Stage Finished"] = true,
+		["URL"] = "https://discord.com/api/webhooks/1016102161689612338/sjdnR-tIozI-JO_gFSy9nZ7G2PV51hJdH7jn4sK4chO7sM4Ab5igib97uQZ6azN1WyX2"
+	},
+	["Legend Stage Joiner"] = {
+		["Stage"] = "Sand Village",
+		["Auto Join"] = true,
+		["Act"] = "Act3"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "ObitaEvo",
+		["Play"] = true,
+		["Ignore Macro Timing"] = true
+	},
+	["Auto Skip Wave"] = true,
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	}
+}
