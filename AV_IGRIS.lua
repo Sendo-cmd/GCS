497732e8dc9bb85241ffec0501f4b329792f9a74
SenDo <champandbank1@gmail.com>
2024-09-10 20:36:12 +0700

    Update AV_IGRIS.lua

--- AV_IGRIS.lua
+++ AV_IGRIS.lua
@@ -1,28 +1,28 @@
 
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1282774939019116564/Igrisfarm.json?ex=66e094c2&is=66df4342&hm=97836621c54317e28a76d96b51b9b6419d2db910b971f3d6259060dda1e054ea&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1283049040765915206/1283049041034477618/hatdog.json?ex=66e19409&is=66e04289&hm=484c6fdda161e45ed61c82e3090b8863c2d7210e65f0a035b790735c937c80ec&"
 getgenv().Config = {
-	["Macros"] = {
-		["Macro"] = "Igrisfarm",
-		["Play"] = true
-	},
 	["AutoSave"] = true,
-	["Match Finished"] = {
-		["Auto Replay"] = true
-	},
 	["Legend Stage Joiner"] = {
-		["Stage"] = "Double Dungeon",
 		["Auto Join"] = true,
+		["Stage"] = "Double Dungeon",
 		["Act"] = "Act3"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
 	},
 	["Performance"] = {
 		["Delete Map"] = true,
 		["Boost FPS"] = true
 	},
 	["Auto Skip Wave"] = true,
-	["Failsafe"] = {
-		["Auto Rejoin"] = true,
-		["Teleport Lobby if Player"] = true
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "hatdog",
+		["Play"] = true
 	}
 }
 getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
