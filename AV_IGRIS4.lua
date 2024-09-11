b974e69b399046ee8d2208b9397c774c0d8bfc5a
SenDo <champandbank1@gmail.com>
2024-09-11 10:33:46 +0700

    Create AV_IGRIS4.lua

---
+++ AV_IGRIS4.lua
@@ -0,0 +1,29 @@
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283269074914119733/VogitanDDNoevo.txt?ex=66e260f5&is=66e10f75&hm=cf9cff83e5f118b053c7cae9949306ed970cfa6f0779240aed362b7162419dad&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Legend Stage Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Double Dungeon",
+		["Act"] = "Act3"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Auto Skip Wave"] = true,
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "hatdogNoro",
+		["Play"] = true,
+    ["Ignore Macro Timing"] = true
+	}
+}
+getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(10)until Joebiden
