6820b7ec66ccfee25ce9a204528c3375d6706ff8
SenDo <champandbank1@gmail.com>
2024-09-11 13:46:05 +0700

    Create AV_IGRIS6.lua

---
+++ AV_IGRIS6.lua
@@ -0,0 +1,24 @@
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Legend Stage Joiner"] = {
+		["Stage"] = "Double Dungeon",
+		["Auto Join"] = true,
+		["Act"] = "Act3"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Auto Skip Wave"] = true,
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "hatdogchain",
+		["Play"] = true
+	}
+}
