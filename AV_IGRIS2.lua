ed20549ad0a3e834f25e4dd3205c9010d71680b4
SenDo <champandbank1@gmail.com>
2024-09-11 10:09:25 +0700

    Update AV_IGRIS2.lua

--- AV_IGRIS2.lua
+++ AV_IGRIS2.lua
@@ -2,6 +2,10 @@ getgenv().EquipMacroTroop = true
 getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283147768881352724/hatdogNoro.json?ex=66e1effb&is=66e09e7b&hm=f145fc3f328246e5d0c91befb003e0116bbe0e15820cab4548ce80164facd951&"
 getgenv().Config = {
 	["AutoSave"] = true,
+	["Webhook"] = {
+		["Stage Finished"] = false,
+		["URL"] = "https://discord.com/api/webhooks/1016102161689612338/sjdnR-tIozI-JO_gFSy9nZ7G2PV51hJdH7jn4sK4chO7sM4Ab5igib97uQZ6azN1WyX2"
+	},
 	["Legend Stage Joiner"] = {
 		["Auto Join"] = true,
 		["Stage"] = "Double Dungeon",
