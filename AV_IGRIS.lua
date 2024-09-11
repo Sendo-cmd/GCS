225518ff96af1b1e885d4a63a9e4eca0774e06bd
SenDo <champandbank1@gmail.com>
2024-09-12 03:16:50 +0700

    Update AV_IGRIS.lua

--- AV_IGRIS.lua
+++ AV_IGRIS.lua
@@ -1,27 +1,28 @@
 getgenv().EquipMacroTroop = true
 getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283058514591486084/hatdog.json?ex=66e19cdb&is=66e04b5b&hm=6cdcf00f3b2091ed46f6b0870c51142b20e6180f84dbacfdcfda524d4dc1a089&"
 getgenv().Config = {
-	["AutoSave"] = true,
 	["Legend Stage Joiner"] = {
-		["Auto Join"] = true,
 		["Stage"] = "Double Dungeon",
+		["Auto Join"] = true,
 		["Act"] = "Act3"
 	},
 	["Failsafe"] = {
 		["Auto Rejoin"] = true,
 		["Teleport Lobby if Player"] = true
 	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true
+	["AutoSave"] = true,
+	["Macros"] = {
+		["Macro"] = "LSDD3",
+		["Ignore Macro Timing"] = true,
+		["Play"] = true
 	},
 	["Auto Skip Wave"] = true,
 	["Match Finished"] = {
 		["Auto Replay"] = true
 	},
-	["Macros"] = {
-		["Macro"] = "hatdog",
-		["Play"] = true
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
 	}
 }
 getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
