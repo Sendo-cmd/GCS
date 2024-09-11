1ba852761774629eb84d5cd57793d2a9cb876aee
SenDo <champandbank1@gmail.com>
2024-09-11 18:41:45 +0700

    Create AV_IGRIS7.lua

---
+++ AV_IGRIS7.lua
@@ -0,0 +1,28 @@
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283390576514961479/hatdognoevo.json?ex=66e2d21d&is=66e1809d&hm=3b241882891626ee5f351337854e8043c55fdb4bf7757ab1d894d1f31a9bc701&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Legend Stage Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Double Dungeon",
+		["Act"] = "Act3"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Auto Skip Wave"] = true,
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "hatdog",
+		["Play"] = true
+	}
+}
+getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(10)until Joebiden
