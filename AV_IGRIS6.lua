0f28af20ee8b83a6d264d85bad96bca9699bbb70
SenDo <champandbank1@gmail.com>
2024-09-11 13:46:55 +0700

    Update AV_IGRIS6.lua

--- AV_IGRIS6.lua
+++ AV_IGRIS6.lua
@@ -1,3 +1,5 @@
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283310339080982569/hatdogchain.json?ex=66e28763&is=66e135e3&hm=b7dea828c24f0b36c2eee79525908249346b9055bc25a3294cdc72596adc5fda&"
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Legend Stage Joiner"] = {
@@ -22,3 +24,5 @@ getgenv().Config = {
 		["Play"] = true
 	}
 }
+getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(10)until Joebiden
