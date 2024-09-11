9518568c99bac5a3c127d098d35f13ad5de9b178
SenDo <champandbank1@gmail.com>
2024-09-11 22:18:17 +0700

    Create AV_NARUTOCH.lua

---
+++ AV_NARUTOCH.lua
@@ -0,0 +1,28 @@
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283374076768948334/NarutoEvoCh.json?ex=66e2c2bf&is=66e1713f&hm=4ced0843f6f07d70efb330657dbbe5249157518271b100b5ee155ab98381ea24&"
+getgenv().Config = {
+	["Legend Stage Joiner"] = {
+		["Stage"] = "Sand Village",
+		["Auto Join"] = true,
+		["Act"] = "Act1"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["AutoSave"] = true,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Auto Skip Wave"] = true,
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "NarutoEvoCh",
+		["Play"] = true
+	}
+}
+getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
