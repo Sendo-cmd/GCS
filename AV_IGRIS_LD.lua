bfdf9f377658f42f6a3fe26488fa2b46858853ea
SenDo <champandbank1@gmail.com>
2024-09-12 03:17:15 +0700

    Update and rename AV_IGRIS2.lua to AV_IGRIS_LD.lua

--- AV_IGRIS_LD.lua
+++ AV_IGRIS_LD.lua
@@ -1,31 +1,27 @@
 getgenv().EquipMacroTroop = true
 getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283147768881352724/hatdogNoro.json?ex=66e1effb&is=66e09e7b&hm=f145fc3f328246e5d0c91befb003e0116bbe0e15820cab4548ce80164facd951&"
 getgenv().Config = {
-	["AutoSave"] = true,
-	["Webhook"] = {
-		["Stage Finished"] = true,
-		["URL"] = "https://discord.com/api/webhooks/1016102161689612338/sjdnR-tIozI-JO_gFSy9nZ7G2PV51hJdH7jn4sK4chO7sM4Ab5igib97uQZ6azN1WyX2"
-	},
 	["Legend Stage Joiner"] = {
-		["Auto Join"] = true,
 		["Stage"] = "Double Dungeon",
+		["Auto Join"] = true,
 		["Act"] = "Act3"
 	},
 	["Failsafe"] = {
 		["Auto Rejoin"] = true,
 		["Teleport Lobby if Player"] = true
 	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true
+	["AutoSave"] = true,
+	["Macros"] = {
+		["Macro"] = "LSDD3LD",
+		["Play"] = true
 	},
 	["Auto Skip Wave"] = true,
 	["Match Finished"] = {
 		["Auto Replay"] = true
 	},
-	["Macros"] = {
-		["Macro"] = "hatdogNoro",
-		["Play"] = true
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
 	}
 }
 getgenv().Key = "991de35c3d962501b5befd2a8d6428b3a245c052ff43f2d37d44bffec7154938"
