64b997bea35c87a070393a884d340582773b24a2
SenDo <champandbank1@gmail.com>
2024-09-14 01:50:41 +0700

    Update AV_IGRIS_ChainE.lua

--- AV_IGRIS_ChainE.lua
+++ AV_IGRIS_ChainE.lua
@@ -2,7 +2,7 @@ game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283542125098565743/LSDD3_ChainE.json?ex=66e35f41&is=66e20dc1&hm=3f4f833d48f65188ec2d0cc5e76bcdbf430e80e4c4e329bf2a4c24e2b6187c6b&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223982782119967/1284223984140947589/LSDD3_ChainE.json?ex=66e5da49&is=66e488c9&hm=2e8aacd81f6a9b909938115df87868b1007e265749ae73cd08e3553fb475f1ec&"
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Legend Stage Joiner"] = {
