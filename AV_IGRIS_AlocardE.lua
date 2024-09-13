ebc6a6167939b00c5d5a9cd597e01c389f71616f
SenDo <champandbank1@gmail.com>
2024-09-14 01:50:09 +0700

    Update AV_IGRIS_AlocardE.lua

--- AV_IGRIS_AlocardE.lua
+++ AV_IGRIS_AlocardE.lua
@@ -2,7 +2,7 @@ game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1283725548635422824/LSDD3_AlocardE.json?ex=66e40a15&is=66e2b895&hm=5db6c110d009ca53f9c60d6692aa44c41785890e4c95156cb799969787c566c9&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223982782119967/1284223983218200657/LSDD3_AlocardE.json?ex=66e5da49&is=66e488c9&hm=4e7f5cbc81e11cb17c5b1322b6e0b1de80da6f542a786480d1f8dd9df09abb2b&"
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Legend Stage Joiner"] = {
