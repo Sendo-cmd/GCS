c18d349fb00b2b921f92d469fd0c0244484fcc3c
SenDo <champandbank1@gmail.com>
2024-09-16 12:31:03 +0700

    Update AV_GEM2.lua

--- AV_GEM2.lua
+++ AV_GEM2.lua
@@ -6,7 +6,7 @@ getgenv().EquipMacroTroop = true
 getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223416110415964/1285110553257447536/GEM2.json?ex=66e913f8&is=66e7c278&hm=339dd9d214918386cc95b80a9574bd015047990b4cfba9017f2423adb850bd08&"
 getgenv().Config = {
 	["Macros"] = {
-		["Macro"] = "520Namak",
+		["Macro"] = "GEM2",
 		["Play"] = true,
 		["Ignore Macro Timing"] = true
 	},
