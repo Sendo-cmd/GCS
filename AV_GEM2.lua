7e0c9c17dbe6d4adc98e058a5fd3304d8d7bbd2b
SenDo <champandbank1@gmail.com>
2024-09-16 12:30:50 +0700

    Update AV_GEM2.lua

--- AV_GEM2.lua
+++ AV_GEM2.lua
@@ -3,7 +3,7 @@ game:GetService("RunService"):Set3dRenderingEnabled(false)
 
 getgenv().RedeemAllCode = true
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/774011709358080021/1282909806738538517/520Namak.txt?ex=66e1bb1d&is=66e0699d&hm=d5efc3b0288008434a847cd00ae979e78ff2ddd097ac601b47757cea51a3146f&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223416110415964/1285110553257447536/GEM2.json?ex=66e913f8&is=66e7c278&hm=339dd9d214918386cc95b80a9574bd015047990b4cfba9017f2423adb850bd08&"
 getgenv().Config = {
 	["Macros"] = {
 		["Macro"] = "520Namak",
