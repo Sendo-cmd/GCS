745b251b2dd8f27dcaa1cf4c120dc715bd850086
SenDo <champandbank1@gmail.com>
2024-09-17 11:57:56 +0700

    Update AV_IGRIS_NarutoE.lua

--- AV_IGRIS_NarutoE.lua
+++ AV_IGRIS_NarutoE.lua
@@ -4,7 +4,7 @@ game:GetService("RunService"):Set3dRenderingEnabled(false)
 getgenv().EquipMacroUnit = true
 getgenv().RedeemAllCode = true
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223982782119967/1284223984971546655/LSDD3_NarutoE.json?ex=66e92609&is=66e7d489&hm=d32ab6b9ea38726a8de306457a20c378e196c62d15edadf23d24768479393562&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223982782119967/1284223984971546655/LSDD3_NarutoE.json?ex=66e9cec9&is=66e87d49&hm=0de49a261d2c186d6e1f183a01392323f68e3f1989c10d51861d2e9c7d9705de&"
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Legend Stage Joiner"] = {
