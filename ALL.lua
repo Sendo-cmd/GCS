c444f9379e32ee9df17293a0cb07fa0530ab55ea
SenDo <champandbank1@gmail.com>
2024-09-17 23:13:37 +0700

    Update ALL.lua

--- ALL.lua
+++ ALL.lua
@@ -2,6 +2,6 @@ repeat task.wait() until game:IsLoaded()
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 
-getgenv().Key = "MARU-Q16F5-3GADI-154S-40AZO-NC7ZL"
-getgenv().id = "423872055499292673"
+getgenv().Key = "MARU-E718N-RO0S-GMW2H-36D2-2XEPG"
+getgenv().id = "338993710278639626"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
