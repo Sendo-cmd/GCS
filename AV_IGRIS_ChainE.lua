ed081cc06b968950343724385cec9aa65d28c671
SenDo <champandbank1@gmail.com>
2024-09-17 13:12:26 +0700

    Update AV_IGRIS_ChainE.lua

--- AV_IGRIS_ChainE.lua
+++ AV_IGRIS_ChainE.lua
@@ -17,7 +17,7 @@ getgenv().Config = {
 		["Teleport Lobby if Player"] = true
 	},
 	["Webhook"] = {
-		["Stage Finished"] = true,
+		["Stage Finished"] = false,
 		["URL"] = "https://discord.com/api/webhooks/1016102161689612338/sjdnR-tIozI-JO_gFSy9nZ7G2PV51hJdH7jn4sK4chO7sM4Ab5igib97uQZ6azN1WyX2"
 	},
 	["Joiner Cooldown"] = 5,
