2fc90dd75dbc58cbbb095c164cc71cc190438acd
SenDo <champandbank1@gmail.com>
2024-09-20 13:42:31 +0700

    Update AV_GEM2.lua

--- AV_GEM2.lua
+++ AV_GEM2.lua
@@ -4,7 +4,7 @@ game:GetService("RunService"):Set3dRenderingEnabled(false)
 getgenv().EquipMacroUnit = true
 getgenv().RedeemAllCode = true
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223416110415964/1285110553257447536/GEM2.json?ex=66ed0878&is=66ebb6f8&hm=e69e9c9cd64855b2e28cd0b376eee2e01fe2ca3119d632c9b6b526fcb27aacb3&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223416110415964/1285110553257447536/GEM2.json?ex=66ee59f8&is=66ed0878&hm=7010e6d4bd3a47b40c7e26915aa52c1f6945a6253be6a65d42ba2cc355c34716&"
 getgenv().Config = {
 	["Macros"] = {
 		["Macro"] = "GEM2",
