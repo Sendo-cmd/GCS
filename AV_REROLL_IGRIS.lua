84dfbbdc6cea211f16f01c74f836c5fb5aaeb4be
SenDo <champandbank1@gmail.com>
2024-09-20 13:26:28 +0700

    Update AV_REROLL_IGRIS.lua

--- AV_REROLL_IGRIS.lua
+++ AV_REROLL_IGRIS.lua
@@ -5,7 +5,7 @@ getgenv().EquipMacroUnit = true
 getgenv().RedeemAllCode = true
 getgenv().EquipMacroTroop = true
 getgenv().ImportMacro = {
-    "https://cdn.discordapp.com/attachments/1286348626259611658/1286348626582306847/C_Dung.json?ex=66ed9503&is=66ec4383&hm=4ec7c9ce1268084f96a55c8041bcdc1359d6ced1493d3d53cd8241a78bd3577e&",
+    "https://cdn.discordapp.com/attachments/1286348626259611658/1286574002604867676/C_Dung.json?ex=66ee66e9&is=66ed1569&hm=c2b875923992bad4cca013652fbf393e49ec5a415d8f1e2410889b52beeb6ded&",
     "https://cdn.discordapp.com/attachments/1286348626259611658/1286348627085889637/C_Namke.json?ex=66ed9503&is=66ec4383&hm=cbeac83d890a10dc4879a6296f6a277fcfbaa74397d9799061c56b5cb9273495&",
     "https://cdn.discordapp.com/attachments/1286348626259611658/1286348627437944842/C_Sand.json?ex=66ed9503&is=66ec4383&hm=74799a89b3a7546e98b0fde9e29fa9593836ed9f28bea03b54f17b671630ca74&",
     "https://cdn.discordapp.com/attachments/1284223982782119967/1285088790662086706/LSDD3_SonjE.json?ex=66ecf433&is=66eba2b3&hm=d859b10afe306444f397c583a4038bfff7a1591abb794c2f2afdcd678bf95128&",
