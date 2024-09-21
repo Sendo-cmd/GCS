f94f035cd2d0a49f0fc86bc0ddbfea7c5e6a83a7
SenDo <champandbank1@gmail.com>
2024-09-21 13:09:07 +0700

    Update AV_REROLL_GEM.lua

--- AV_REROLL_GEM.lua
+++ AV_REROLL_GEM.lua
@@ -6,7 +6,7 @@ getgenv().RedeemAllCode = true
 getgenv().EquipMacroTroop = true
 getgenv().ImportMacro = {
     "https://cdn.discordapp.com/attachments/1286348626259611658/1286574002604867676/C_Dung.json?ex=66ef0fa9&is=66edbe29&hm=05fe7d24943d921d5e65fac0eeaad330b654f9a7076d34096cbb5402000d9eea&",
-    "https://cdn.discordapp.com/attachments/1286348626259611658/1286348627085889637/C_Namke.json?ex=66ef8f43&is=66ee3dc3&hm=ce06f046be77d3051568df11d965317068a0caf483bb1df55084927c26dc13c9&",
+    "https://cdn.discordapp.com/attachments/1286348626259611658/1286932039211421736/C_Namke.json?ex=66efb45c&is=66ee62dc&hm=5d712c3d6f153f76971d4b38712006fc05c9557d835edc28e78a56634630e7ac&",
     "https://cdn.discordapp.com/attachments/1286348626259611658/1286348627437944842/C_Sand.json?ex=66ef8f43&is=66ee3dc3&hm=d4024a8188b45bd4ccee698af4958cf24bb5890d5c5905e1ad5067c7d57c503a&",
     "https://cdn.discordapp.com/attachments/1284223416110415964/1284223416244895775/GEM.json?ex=66ef1442&is=66edc2c2&hm=3f7f718294e4711c1bb12cc7b554df1b6d35ccf28a96a3616a4bb10cac90fbcd&",
 }
