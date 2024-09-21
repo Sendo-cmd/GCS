8f54e42299dddaf7778f9502a3bbcb64d7b03f3c
SenDo <champandbank1@gmail.com>
2024-09-22 03:32:42 +0700

    Update AV_IGRIS_SonjE.lua

--- AV_IGRIS_SonjE.lua
+++ AV_IGRIS_SonjE.lua
@@ -4,7 +4,7 @@ game:GetService("RunService"):Set3dRenderingEnabled(false)
 getgenv().EquipMacroUnit = true
 getgenv().RedeemAllCode = true
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223982782119967/1285088790662086706/LSDD3_SonjE.json?ex=66ef9733&is=66ee45b3&hm=56994b1c0272e88011db7ee8ba205326d8d01240289e36bf5017b77d14648a0e&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223982782119967/1285088790662086706/LSDD3_SonjE.json?ex=66f03ff3&is=66eeee73&hm=2345a8f46c7eaac9e885c62a51345f0d8c39b0f856446dc572458f8b3067f556&"
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Legend Stage Joiner"] = {
