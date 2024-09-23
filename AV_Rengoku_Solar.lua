81f79cadf63a71979211f0111c633abfa1a66477
Sendo_cmd <champandbank1@gmail.com>
2024-09-23 17:10:24 +0700

    aaaaaaaaaa

---
+++ AV_Rengoku_Solar.lua
@@ -0,0 +1,52 @@
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+getgenv().EquipMacroUnit = true
+getgenv().RedeemAllCode = true
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1286348626259611658/1286574002604867676/C_Dung.json?ex=66f06129&is=66ef0fa9&hm=d3bb2c406b0b5c83bba734251fbde0620d1663cd57c8c7af79764acede4757f4&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Joiner Cooldown"] = 10,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Raid Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Spider Forest",
+		["Act"] = "Act4"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true
+	},
+	["Gameplay"] = {
+		["Auto Skip Wave"] = true,
+		["Auto Sell"] = {
+			["Wave"] = 1
+		}
+	},
+	["Macros"] = {
+		["Play"] = true,
+		["Macro"] = "Raid_Solar_Vogita",
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	}
+}
+getgenv().Key = "k517c79e9160307a9b87210d"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
