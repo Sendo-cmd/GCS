471a246350585905029cc7ebab3bee8d84d7df1b
SenDo <champandbank1@gmail.com>
2024-09-24 01:17:58 +0700

    Create AV_Rengoku_Base.lua

---
+++ AV_Rengoku_Base.lua
@@ -0,0 +1,52 @@
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+getgenv().EquipMacroUnit = true
+getgenv().RedeemAllCode = true
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1287719591354826847/1287840124524171264/Raid_Base.json?ex=66f30214&is=66f1b094&hm=b7ff650056145f20c19c3bb1efaa730a404b4c1ce5fc60aaa04689cf0d029aac&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Joiner Cooldown"] = 10,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Raid Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Spider Forest",
+		["Act"] = "Act4"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true
+	},
+	["Gameplay"] = {
+		["Auto Skip Wave"] = true,
+		["Auto Sell"] = {
+			["Wave"] = 1
+		}
+	},
+	["Macros"] = {
+		["Play"] = true,
+		["Macro"] = "Raid_Base",
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	}
+}
+getgenv().Key = "k517c79e9160307a9b87210d"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
