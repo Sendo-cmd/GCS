784e24fa5b4e6d1eeed68d06cedea2e3ec1aa97a
Sendo_cmd <champandbank1@gmail.com>
2024-09-23 18:39:37 +0700

    aaaaaaaaaaa

---
+++ AV_Rengoku_Igris.lua
@@ -0,0 +1,52 @@
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+getgenv().EquipMacroUnit = true
+getgenv().RedeemAllCode = true
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1287719591354826847/1287739683538735215/Raid_Igris.json?ex=66f2a489&is=66f15309&hm=5488baff2db72b0c2c68d7a1a390af800790bfddeec207e1b82da3b8441e18be&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Joiner Cooldown"] = 10,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Raid Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Spider Forest",
+		["Act"] = "Act4"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true
+	},
+	["Gameplay"] = {
+		["Auto Skip Wave"] = true,
+		["Auto Sell"] = {
+			["Wave"] = 1
+		}
+	},
+	["Macros"] = {
+		["Play"] = true,
+		["Macro"] = "Raid_Igris",
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	}
+}
+getgenv().Key = "k517c79e9160307a9b87210d"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
