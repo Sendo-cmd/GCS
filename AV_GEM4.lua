484e1da4893ed4da2db139057ce41ee31e0ac2d3
SenDo <champandbank1@gmail.com>
2024-09-23 20:26:28 +0700

    Create AV_GEM4.lua

---
+++ AV_GEM4.lua
@@ -0,0 +1,47 @@
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+getgenv().EquipMacroUnit = true
+getgenv().RedeemAllCode = true
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223416110415964/1285244706502213632/GEM3.json?ex=66f0d128&is=66ef7fa8&hm=ecf16e7a33f5c464d75d77d0439e70e4f3a86bda7f7d95eef2b0d647b655d8c4&"
+getgenv().Config = {
+	["Macros"] = {
+		["Macro"] = "GEM3",
+		["Play"] = true,
+		["Ignore Macro Timing"] = true,
+		["Walk Around"] = true
+	},
+	["AutoSave"] = true,
+	["Stage Joiner"] = {
+		["Auto Join"] = true,
+		["Act"] = "Act1",
+		["Stage"] = "Planet Namak"
+	},
+	["Webhook"] = {
+		["Stage Finished"] = false,
+		["URL"] = "https://discord.com/api/webhooks/1285656259604774912/2m06zwdD_zylzlJ6iZ7F167cgEd8Vbg8leUk1obYgKVjXMctXsaxPIoB2FTpmkntDGnj"
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Daily Reward"] = true
+	},
+	["Joiner Cooldown"] = 10,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true
+	},
+	["Auto Skip Wave"] = true,
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	}
+}
+getgenv().Key = "k517c79e9160307a9b87210d"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
