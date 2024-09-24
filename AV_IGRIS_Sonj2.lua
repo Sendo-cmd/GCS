a5f143ff19965115176f707a3a84dbfe1911ce53
Sendo_cmd <champandbank1@gmail.com>
2024-09-24 22:22:36 +0700

    aaaaaaaaaaaa

---
+++ AV_IGRIS_Sonj2.lua
@@ -0,0 +1,53 @@
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+
+getgenv().EquipMacroUnit = true
+getgenv().RedeemAllCode = true
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1284223982782119967/1288158553667797013/LSDD3_Sonj2.json?ex=66f42aa4&is=66f2d924&hm=4ba0a7ef567a4b308d9101868bc7cf7c09acfc2a6e0ff001ce18eb6a79590489&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Legend Stage Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Double Dungeon",
+		["Act"] = "Act3"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Joiner Cooldown"] = 10,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = {
+			["Enable"] = true,
+			["FPS Cap"] = 20
+		}
+	},
+	["Auto Skip Wave"] = true,
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Webhook"] = {
+		["Stage Finished"] = false,
+		["URL"] = "https://discord.com/api/webhooks/1285656259604774912/2m06zwdD_zylzlJ6iZ7F167cgEd8Vbg8leUk1obYgKVjXMctXsaxPIoB2FTpmkntDGnj"
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Daily Reward"] = true
+	},
+	["Macros"] = {
+		["Macro"] = "LSDD3_Sonj2",
+		["Play"] = true,
+		["Walk Around"] = true,
+		["Ignore Macro Timing"] = false
+	}
+}
+getgenv().Key = "k517c79e9160307a9b87210d"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
