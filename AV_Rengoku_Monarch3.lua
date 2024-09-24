82610643b334e7c168e4d9560529a457b4ac2338
Sendo_cmd <champandbank1@gmail.com>
2024-09-25 01:41:31 +0700

    aaaaaaaa

---
+++ AV_Rengoku_Monarch3.lua
@@ -0,0 +1,55 @@
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+
+getgenv().EquipMacroUnit = true
+getgenv().RedeemAllCode = true
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1287719591354826847/1288208679660687452/Raid_Monarch_Ten.json?ex=66f45952&is=66f307d2&hm=0b9c17044d4c6e0881776894810eb4687df23cc0106730ed48970455ced05efd&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Joiner Cooldown"] = 10,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = {
+			["Enable"] = true,
+			["FPS Cap"] = 20
+		}
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Raid Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Spider Forest",
+		["Act"] = "Act4"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true
+	},
+	["Gameplay"] = {
+		["Auto Skip Wave"] = true,
+		["Auto Sell"] = {
+			["Wave"] = 1
+		}
+	},
+	["Macros"] = {
+		["Play"] = true,
+		["Macro"] = "Raid_Monarch_Ten",
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	}
+}
+getgenv().Key = "k517c79e9160307a9b87210d"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
---
+++ AV_Rengoku_Monarch5.lua
@@ -0,0 +1,55 @@
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+
+getgenv().EquipMacroUnit = true
+getgenv().RedeemAllCode = true
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1287719591354826847/1287768182618198046/Raid_Monarch_Song.json?ex=66f367d4&is=66f21654&hm=2d3629ea338782be7b68851175f7a3ffd508ba44f56b744e1e86b75b7307491e&"
+getgenv().Config = {
+	["AutoSave"] = true,
+	["Joiner Cooldown"] = 10,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = {
+			["Enable"] = true,
+			["FPS Cap"] = 20
+		}
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Raid Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Spider Forest",
+		["Act"] = "Act4"
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true
+	},
+	["Gameplay"] = {
+		["Auto Skip Wave"] = true,
+		["Auto Sell"] = {
+			["Wave"] = 1
+		}
+	},
+	["Macros"] = {
+		["Play"] = true,
+		["Macro"] = "Raid_Monarch_Song",
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	}
+}
+getgenv().Key = "k517c79e9160307a9b87210d"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
