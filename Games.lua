28ce318b5ce67ea04a224370216aa0c5d71fd42e
SenDo <champandbank1@gmail.com>
2024-09-25 17:42:00 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -220,7 +220,7 @@ Accounts = {
 
 }
 
-repeat task.wait() until game:IsLoaded()
+repeat task.wait(3) until game:IsLoaded()
 
 local plr = game.Players.LocalPlayer
 local Scripts = Games[game.gameId]
