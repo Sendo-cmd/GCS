1a4ac5723b88cbe6cd588c3b086cc78f54587f4b
SenDo <champandbank1@gmail.com>
2024-09-26 17:36:28 +0700

    Update LobbyAV.lua

--- LobbyAV.lua
+++ LobbyAV.lua
@@ -2,8 +2,6 @@ local GameLoad = nil
 local PlaceId = {}
 if game.PlaceId == 16146832113 then
     GameLoad = "autodeleta"
-elseif game.PlaceId == 16146832113 then
-    GameLoad = "autodeleta"
 end
 local var,err = pcall(function ()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Sendo-cmd/GCS/main/" .. GameLoad .. ".lua"))()
