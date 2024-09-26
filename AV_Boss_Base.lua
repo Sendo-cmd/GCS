dc434926b74445b3d935f5e363e7f79781ec6cc4
SenDo <champandbank1@gmail.com>
2024-09-27 00:42:41 +0700

    Create AV_Boss_Base.lua

---
+++ AV_Boss_Base.lua
@@ -0,0 +1,43 @@
+getgenv().Config = {
+	["Joiner Cooldown"] = 10,
+	["Match Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Boss Event Joiner"] = {
+		["Auto Join"] = true
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = {
+			["Enable"] = true,
+			["FPS Cap"] = 10
+		}
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["AutoSave"] = true,
+	["Claimer"] = {
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Daily Reward"] = true
+	},
+	["Gameplay"] = {
+		["Auto Skip Wave"] = true
+	},
+	["Macros"] = {
+		["Ignore Macro Timing"] = true,
+		["Macro"] = "Boss_Base",
+		["Play"] = true,
+		["No Ignore Sell Timing"] = true
+	}
+}
