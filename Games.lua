437666ffda9f21d264daaa72d83047d642076822
Sendo_cmd <champandbank1@gmail.com>
2024-10-07 12:36:57 +0700

    a

--- Games.lua
+++ Games.lua
@@ -388,7 +388,7 @@ Accounts = {
 
 }
 
-repeat task.wait() until game:IsLoaded()
+repeat task.wait(2) until game:IsLoaded()
 
 local plr = game.Players.LocalPlayer
 local Scripts = Games[game.gameId]
@@ -412,6 +412,6 @@ for i,v in pairs(ScriptLists) do
             Loaded = true
         end)
 
-        repeat task.wait() until Loaded or (Timer - tick()) > 10
+        repeat task.wait() until Loaded or (Timer - tick()) > 4
     end
 end
