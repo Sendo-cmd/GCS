3fee02add92a27b1196b47cb922965182de183b8
Sendo_cmd <champandbank1@gmail.com>
2024-10-07 13:38:39 +0700

    a

--- AV_IGRIS_SonjEV2.lua
+++ AV_IGRIS_SonjEV2.lua
@@ -79,4 +79,4 @@ getgenv().Config = {
 	}
 }
 getgenv().Key = "k517c79e9160307a9b87210d"
-repeat wait(1)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
+repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
