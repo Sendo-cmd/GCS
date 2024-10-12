7c507b7c7ec89c6ad33162af9c642f242914c7c3
Sendo_cmd <champandbank1@gmail.com>
2024-10-12 16:04:40 +0700

    a

--- AV_GEM.lua
+++ AV_GEM.lua
@@ -4,19 +4,33 @@ getgenv().EquipMacroUnit = true
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Stage Joiner"] = {
+		["Auto Join"] = true,
 		["Act"] = "Act1",
-		["Stage"] = "Planet Namak",
-		["Auto Join"] = true
+		["Stage"] = "Planet Namak"
+	},
+	["Auto Play"] = {
+		["Auto Upgrade"] = true,
+		["Enable"] = true,
+		["Middle Position"] = {
+			["Planet Namak"] = "135.39906311035156, 7.105718612670898, 121.3156967163086"
+		},
+		["Upgrade Method"] = "Nearest from Middle Position (until Max)"
 	},
 	["Match Finished"] = {
 		["Auto Replay"] = true
 	},
-	["Auto Skip Wave"] = true,
 	["Failsafe"] = {
 		["Auto Rejoin"] = true,
 		["Teleport Lobby if Player"] = true
 	},
-	["Joiner Cooldown"] = 10,
+	["Macros"] = {
+		["No Ignore Sell Timing"] = true,
+		["Ignore Macro Timing"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true,
+		["Random Offset"] = true
+	},
 	["Performance"] = {
 		["Delete Map"] = true,
 		["Boost FPS"] = true,
@@ -29,33 +43,21 @@ getgenv().Config = {
 		["Ping Freeze"] = true,
 		["Teleport Lobby FPS below"] = {
 			["Enable"] = true,
-			["FPS"] = 5
+			["FPS"] = 7
 		}
-	},
-	["Secure"] = {
-		["Walk Around"] = true,
-		["Random Offset"] = true
-	},	
-	["Webhook"] = {
-		["Stage Finished"] = false,
-		["URL"] = ""
 	},
 	["Claimer"] = {
 		["Auto Claim Milestone"] = true,
 		["Auto Claim Quest"] = true,
-		["Auto Claim Achievement"] = true,
+		["Auto Claim Battle Pass"] = true,
 		["Auto Claim Daily Reward"] = true,
 		["Auto Claim Collection"] = true,
 		["Auto Claim Enemy Index"] = true,
-		["Auto Claim Battle Pass"] = true,
-		["Auto Claim Achievement"] = true,
-		["Auto Claim Collection"] = true,
-		["Auto Claim Daily Reward"] = true
+		["Auto Claim Achievement"] = true
 	},
-	["Macros"] = {
-		["Macro"] = "GEM",
-		["Play"] = true,
-		["Ignore Macro Timing"] = true
+	["Gameplay"] = {
+		["Auto Skip Wave"] = true,
+		["Auto Use Ability"] = true
 	}
 }
 getgenv().Key = "k517c79e9160307a9b87210d"
