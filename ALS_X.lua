d16ec3927903d1c4ad471f95672aed4995fa7e02
Sendo_cmd <champandbank1@gmail.com>
2024-10-17 19:39:56 +0700

    a

--- ALS_X.lua
+++ ALS_X.lua
@@ -1,4 +1,2 @@
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-
 getgenv().key = '4487198d-1f05-43ac-9196-c99fe70e687d'
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
