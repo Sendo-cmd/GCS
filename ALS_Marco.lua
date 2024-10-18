47b0cff49b5d1987e8b79bda48975cf2bbc4036c
Sendo_cmd <champandbank1@gmail.com>
2024-10-18 19:07:35 +0700

    a

--- ALS_Marco.lua
+++ ALS_Marco.lua
@@ -2,18 +2,18 @@ local Endpoint = "https://raw.githubusercontent.com/test-macro/ALS_Marco/main/"
 local Profiles = {
     "Ikatsuq.json",
     "FireBlackDevilZ.json",
-};
+}
 
 repeat task.wait(1) until game:IsLoaded()
 
-local PATH = "Xenon_Hub_Anime_Last_Stand/User";
+local PATH = "Xenon Hub Anime Last Stand/User"
 
 -- Process
 
 if not isfolder(PATH) then
     local path = ""
-    for i,v in pairs(PATH:split("/")) do
-        path ..= `{v}/`
+    for i, v in pairs(PATH:split("/")) do
+        path = path .. v .. "/"
         makefolder(path)
     end
 end
@@ -22,10 +22,14 @@ local HttpService = game:GetService("HttpService")
 
 for _, Name in pairs(Profiles) do
     local Profile_Data = game:HttpGet(Endpoint .. Name)
-
-    local success, JSON = pcall(HttpService.JSONDecode, HttpService, Profile_Data)
+    
+    local success, JSON = pcall(function()
+        return HttpService:JSONDecode(Profile_Data)
+    end)
 
     if success then
-        writefile(`{PATH}/{Name}`, Profile_Data)
+        writefile(PATH .. "/" .. Name, Profile_Data)
+    else
+        warn("Failed to decode JSON for profile: " .. Name)
     end
 end
