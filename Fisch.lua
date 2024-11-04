419982efffc9abeccf32a2dc00e0dab9bd75faea
Sendo_cmd <champandbank1@gmail.com>
2024-11-04 21:49:29 +0700

    a

--- Fisch.lua
+++ Fisch.lua
@@ -1,4 +1,28 @@
 repeat task.wait() until game:IsLoaded()
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+getgenv().Config = {
+	["Character"] = {
+		["Disable Swim"] = true
+	},
+	["Auto Sell"] = {
+		["Enable"] = true,
+		["Delay"] = 1
+	},
+	["Performance"] = {
+		["Boost FPS"] = true
+        
+	},
+	["Auto Fish"] = {
+		["Fish Location"] = {
+			["Auto"] = true,
+			["Zone"] = "Desolate Deep"
+		},
+		["Cast Threshold"] = 95,
+		["Perfect Chance"] = 90,
+		["Enable"] = true,
+		["Fail Chance"] = 5
+	},
+	["AutoSave"] = true
+}
 getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
