673a457989607eaab9d0bae0e879f572b8b00b89
Sendo_cmd <champandbank1@gmail.com>
2024-11-04 20:54:22 +0700

    a

--- Fisch.lua
+++ Fisch.lua
@@ -1,4 +1,3 @@
 repeat task.wait() until game:IsLoaded()
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
