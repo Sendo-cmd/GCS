5c8127827720f6fe880534594c6dc0bb153a0d31
Sendo_cmd <champandbank1@gmail.com>
2024-11-07 21:37:57 +0700

    a

--- AV_Trait.lua
+++ AV_Trait.lua
@@ -103,4 +103,4 @@ getgenv().Config = {
 	}
 }
 getgenv().Key = "k517c79e9160307a9b87210d"
-repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(4)until Joebiden
+repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
