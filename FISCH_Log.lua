304e1a6c247cc79a1e6f330da1f1d40fa026d63c
Sendo_cmd <champandbank1@gmail.com>
2024-11-15 23:41:58 +0700

    a

---
+++ FISCH_Log.lua
@@ -0,0 +1,62 @@
+repeat wait() until game:IsLoaded()
+game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local Settings = {
+    ["Cooldown"] = 12
+}
+
+local plr = game:GetService("Players").LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local playerstats = ReplicatedStorage:WaitForChild("playerstats")
+local HttpService = game:GetService("HttpService")
+local Insert_To = {}
+local Last_Send_Data = tick()
+local Index = 0
+local FishCahce = {}
+local Fishs = {}
+for i,v in pairs(playerstats[plr.Name].Inventory:GetChildren()) do
+    table.insert(FishCahce,v.Name)
+end
+game:GetService("ReplicatedStorage").events.anno_catch.OnClientEvent:Connect(function(b)
+    Fishs[tostring(Index)] = b
+    Index = Index + 1
+end)
+
+spawn(function ()
+    while true do task.wait()
+        if tick() >= Last_Send_Data then
+            for i,v in pairs(game:GetService("ReplicatedStorage").world:GetChildren()) do
+                Insert_To[v.Name] = v.Value
+            end
+            local FishCahce = {}
+            for i,v in pairs(playerstats[plr.Name].Inventory:GetChildren()) do
+                FishCahce[v.Name] = {
+                    ["Stack"] = v.Stack.Value,
+                    ["Name"] = v.Value,
+                }
+            end
+            local Example = {
+                ["Method"] = "Update",
+                ["Server_Data"] = Insert_To,
+                ["Username"] = plr.Name,
+                ["Gold"] = plr.leaderstats["C$"].Value,
+                ["Level"] = plr.leaderstats["Level"].Value,
+                ["Bait"] = playerstats[plr.Name].Stats.bait.Value,
+                ["Rod"] = playerstats[plr.Name].Stats.rod.Value,
+                ["Fishs"] = Fishs,
+                ["All_Fish"] = FishCahce,
+                ["GuildId"] = "467359347744309248",
+                ["DataKey"] = "GamingChampionShopAPI",
+            }
+            Index = 0
+            Fishs = {} 
+            
+            local response = request({
+                ["Url"] = "http://champions.thddns.net:3031/logs",
+                ["Method"] = "POST",
+                ["Body"] = HttpService:JSONEncode(Example)
+            })
+            Last_Send_Data = tick() + Settings["Cooldown"]
+        end
+    end
+end)
