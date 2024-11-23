2a00140962e019f62cb746784837dc50d5795804
Sendo_cmd <champandbank1@gmail.com>
2024-11-24 02:13:57 +0700

    a

--- AV_DungBase.lua
+++ AV_DungBase.lua
@@ -2,23 +2,29 @@ game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().RedeemAllCode = true
 getgenv().EquipMacroUnit = true
 getgenv().Config = {
+	["AutoSave"] = true,
+	["Dungeon Joiner"] = {
+		["Stage"] = "Mountain Shrine (Natural)",
+		["Auto Join"] = true,
+		["Act"] = "OccultHunt"
+	},
 	["Joiner Cooldown"] = 10,
 	["Macros"] = {
 		["Macro"] = "Dung_Base",
 		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true,
-		["Play"] = true
+		["Play"] = true,
+		["No Ignore Sell Timing"] = true
 	},
-	["Dungeon Joiner"] = {
-		["Auto Join"] = true,
-		["Stage"] = "Mountain Shrine (Natural)",
-		["Act"] = "OccultHunt"
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
 	},
 	["Match Finished"] = {
 		["Auto Replay"] = true,
 		["Replay Amount"] = 0
 	},
-	["AutoSave"] = true,
 	["Failsafe"] = {
 		["Auto Rejoin"] = true,
 		["Teleport Lobby if Player"] = true
@@ -28,32 +34,21 @@ getgenv().Config = {
 		["Random Offset"] = true
 	},
 	["Gameplay"] = {
-		["Occult Hunt"] = {
-			["Use All Talisman"] = {
-				["Enable"] = true,
-				["Wave"] = 50
-			},
-			["Collect Talisman"] = true,
-			["Use Talisman on Crab"] = true
-		},
-		["Auto Skip Wave"] = true,
-		["Auto Use Ability"] = true,
 		["Auto Modifier"] = {
-			["Enable"] = true,
 			["Prioritize"] = {
 				["Strong"] = 3,
 				["Thrice"] = 4,
 				["Warding off Evil"] = 23,
-				["Champions"] = 96,
+				["Range"] = 92,
 				["Fast"] = 1,
 				["Revitalize"] = 6,
 				["Exploding"] = 2,
 				["Dodge"] = 10,
 				["Immunity"] = 11,
-				["Range"] = 92,
+				["Quake"] = 31,
 				["Damage"] = 98,
 				["Slayer"] = 95,
-				["Quake"] = 31,
+				["Champions"] = 96,
 				["Fisticuffs"] = 9,
 				["Drowsy"] = 8,
 				["Cooldown"] = 22,
@@ -69,20 +64,21 @@ getgenv().Config = {
 				["Uncommon Loot"] = 94,
 				["Money Surge"] = 100
 			},
+			["Enable"] = true,
 			["Amount"] = {
 				["Strong"] = 0,
 				["Thrice"] = 0,
 				["Warding off Evil"] = 0,
-				["Quake"] = 0,
+				["Range"] = 0,
 				["Fast"] = 0,
 				["Revitalize"] = 2,
 				["Exploding"] = 0,
 				["Dodge"] = 0,
 				["Immunity"] = 0,
-				["Range"] = 0,
+				["Champions"] = 0,
 				["Damage"] = 10,
 				["Slayer"] = 10,
-				["Champions"] = 0,
+				["Quake"] = 0,
 				["Fisticuffs"] = 0,
 				["Drowsy"] = 0,
 				["Cooldown"] = 0,
@@ -98,7 +94,17 @@ getgenv().Config = {
 				["Uncommon Loot"] = 10,
 				["Money Surge"] = 0
 			}
-		}
+		},
+		["Occult Hunt"] = {
+			["Collect Talisman"] = true,
+			["Use All Talisman"] = {
+				["Enable"] = true,
+				["Wave"] = 50
+			},
+			["Use Talisman on Crab"] = true
+		},
+		["Auto Skip Wave"] = true,
+		["Auto Use Ability"] = true
 	}
 }
 getgenv().Key = "k517c79e9160307a9b87210d"
