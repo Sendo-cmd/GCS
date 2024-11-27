96316cffb7ce74904c1f9c9360ad4072b0d65488
Sendo_cmd <champandbank1@gmail.com>
2024-11-28 04:56:02 +0700

    a

--- Fischns.lua
+++ Fischns.lua
@@ -50,5 +50,5 @@ getgenv().Config = {
 		["Auto Rejoin"] = false
 	}
 }
-getgenv().Key = "sZoENIPLYqhElsjhrFywrbFsAGeUzAdm"
+getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
