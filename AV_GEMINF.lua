fb1881e89408ff8c8c315b78ce4ab72908b05398
Sendo_cmd <champandbank1@gmail.com>
2024-11-30 01:14:50 +0700

    a

--- AV_GEMINF.lua
+++ AV_GEMINF.lua
@@ -92,5 +92,5 @@ getgenv().Config = {
 		}	
 	}
 }
-getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
+getgenv().Key = "k517c79e9160307a9b87210d"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
--- AV_Rengoku_Base.lua
+++ AV_Rengoku_Base.lua
@@ -60,5 +60,5 @@ getgenv().Config = {
 		["No Ignore Sell Timing"] = true
 	}
 }
-getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
+getgenv().Key = "k517c79e9160307a9b87210d"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
