1b5644338f382e0ac5f61d94a8087e9c9f0eb99c
Sendo_cmd <champandbank1@gmail.com>
2024-12-01 06:27:16 +0700

    a

--- MyFisch.lua
+++ MyFisch.lua
@@ -1,3 +1,54 @@
-repeat task.wait() until game:IsLoaded()
+getgenv().Config = {
+	["Inventory"] = {
+		["Auto Favourite"] = {
+			["Weight"] = 1000,
+			["Enable"] = true,
+			["Unfavourite if unmatch"] = true
+		},
+		["Auto Sell"] = {
+			["Enable"] = true,
+			["Rarity"] = {
+				["Legendary"] = true,
+				["Mythical"] = true
+			},
+			["Delay"] = 2
+		}
+	},
+	["AutoSave"] = true,
+	["Character"] = {
+		["Disable Swim"] = true
+	},
+	["Auto Fish"] = {
+		["Enable"] = true,
+		["Shark Event"] = {
+			["Great Hammerhead Shark"] = true,
+			["Whale Shark"] = true,
+			["The Depths - Serpent"] = true,
+			["Great White Shark"] = true
+		},
+		["Fish Location"] = {
+			["Height"] = 5,
+			["Auto"] = true,
+			["Zone"] = "Ancient Isle Waterfall"
+		},
+		["Cast Threshold"] = 95,
+		["Perfect Chance"] = 50,
+		["Fail Chance"] = 0
+	},
+	["Performance"] = {
+		["Fullbright"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true
+	},
+	["Auto Aurora Totem"] = {
+		["Use Sundial if Day"] = true,
+		["Enable"] = true
+	},
+	["Performance Failsafe"] = {
+		["Rejoin Timer"] = {
+			["Minute"] = 60
+		}
+	}
+}
 getgenv().Key = "k517c79e9160307a9b87210d"
-repeat wait(0)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))() 
