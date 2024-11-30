dae40d367728a44651f95aeb5bbd68dfc2cf4759
Sendo_cmd <champandbank1@gmail.com>
2024-12-01 02:56:19 +0700

    a

--- AV_CF_Obita.lua
+++ AV_CF_Obita.lua
@@ -75,5 +75,5 @@ getgenv().Config = {
 		["Upgrade Method"] = "Nearest from Middle Position (until Max)"
 	}
 }
-getgenv().Key = "k517c79e9160307a9b87210d"
+getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
