40e9ec7fa293507d548ad07f61c78c09d0d86a52
Sendo_cmd <champandbank1@gmail.com>
2024-12-01 06:07:58 +0700

    a

--- MyFisch.lua
+++ MyFisch.lua
@@ -1,3 +1,3 @@
 repeat task.wait() until game:IsLoaded()
 getgenv().Key = "k517c79e9160307a9b87210d"
-repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
+repeat wait(0)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
