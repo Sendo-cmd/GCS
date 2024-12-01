c5928783222089ed83b4f322bf9e4481587751cd
Sendo_cmd <champandbank1@gmail.com>
2024-12-02 00:41:02 +0700

    a

--- MyFisch.lua
+++ MyFisch.lua
@@ -51,6 +51,5 @@ getgenv().Config = {
 		}
 	}
 }
-getgenv().Key = "k517c79e9160307a9b87210d"
-print("executing script")
+getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
