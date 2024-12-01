010452494916a2520d7e0f0189087a3175e76c5c
Sendo_cmd <champandbank1@gmail.com>
2024-12-02 03:55:08 +0700

    a

--- AV_Paragon25.lua
+++ AV_Paragon25.lua
@@ -65,5 +65,5 @@ getgenv().Config = {
 		["Auto Use Ability"] = true
 	}
 }
-getgenv().Key = "k517c79e9160307a9b87210d"
+getgenv().Key = "wPfsNXRRMazroqcwmxPIUwPcGvwkRZer"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
