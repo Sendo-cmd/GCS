3b133be1c5ba3248b2667f8b520bcd1f26b81c60
Sendo_cmd <champandbank1@gmail.com>
2024-12-08 15:21:28 +0700

    a

--- AV_DungBase.lua
+++ AV_DungBase.lua
@@ -122,4 +122,4 @@ getgenv().Config = {
 	}
 }
 getgenv().Key = "k517c79e9160307a9b87210d"
-repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(4)until Joebiden
+repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(5)until Joebiden
