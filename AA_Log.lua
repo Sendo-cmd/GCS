6ac9e9727fe9da22b05bb8369f8b2a75e8d15080
Sendo_cmd <champandbank1@gmail.com>
2025-01-04 18:08:56 +0700

    a

--- AA_Log.lua
+++ AA_Log.lua
@@ -5,6 +5,7 @@ game:GetService("Players").LocalPlayer.PlayerGui.spawn_units:WaitForChild("Lives
 local plr = game:GetService("Players").LocalPlayer
 local HttpService = game:GetService("HttpService")
 if game.PlaceId == 8304191830  then
+    local session = require(game.ReplicatedStorage.src.Loader).load_client_service(game:GetService("Players").LocalPlayer.PlayerScripts.main, "UnitCollectionServiceClient")["session"]
     local response = request({
         ["Url"] = "https://api.championshop.date/log-aa",
         ["Method"] = "POST",
@@ -15,7 +16,7 @@ if game.PlaceId == 8304191830  then
             ["Method"] = "Update",
             ["Place"] = "Lobby",
             ["Username"] = plr.Name,
-            ["session"] = game:GetService("HttpService"):JSONEncode(require(game.ReplicatedStorage.src.Loader).load_client_service(game:GetService("Players").LocalPlayer.PlayerScripts.main, "UnitCollectionServiceClient")["session"]),
+            ["inventory"] = game:GetService("HttpService"):JSONEncode(session["inventory"]["inventory_profile_data"]),
             ["Gold"] = plr._stats.gold_amount.Value,
             ["Gem"] =  plr._stats.gem_amount.Value,
             ["Level"] = game.Players.LocalPlayer.PlayerGui["spawn_units"].Lives.Main.Desc.Level.Text:split('Level ')[2],
@@ -28,6 +29,7 @@ if game.PlaceId == 8304191830  then
     end
 else
     game:GetService("ReplicatedStorage").endpoints.server_to_client.game_finished.OnClientEvent:Connect(function(g)
+        local session = require(game.ReplicatedStorage.src.Loader).load_client_service(game:GetService("Players").LocalPlayer.PlayerScripts.main, "UnitCollectionServiceClient")["session"]
         local response = request({
             ["Url"] = "https://api.championshop.date/log-aa",
             ["Method"] = "POST",
@@ -38,11 +40,12 @@ else
                 ["Method"] = "Update",
                 ["Place"] = "Game",
                 ["Username"] = plr.Name,
-                ["session"] = game:GetService("HttpService"):JSONEncode(require(game.ReplicatedStorage.src.Loader).load_client_service(game:GetService("Players").LocalPlayer.PlayerScripts.main, "UnitCollectionServiceClient")["session"]),
+                ["inventory"] = game:GetService("HttpService"):JSONEncode(session["inventory"]["inventory_profile_data"]),
                 ["Gold"] = plr._stats.gold_amount.Value,
                 ["Gem"] =  plr._stats.gem_amount.Value,
                 ["Level"] = game.Players.LocalPlayer.PlayerGui["spawn_units"].Lives.Main.Desc.Level.Text:split('Level ')[2],
                 ["Rewards"] = game:GetService("HttpService"):JSONEncode(g),
+                ["MapInfo"] = game:GetService("HttpService"):JSONEncode(workspace._MAP_CONFIG.GetLevelData:InvokeServer()),
                 ["GuildId"] = "467359347744309248",
                 ["DataKey"] = "GamingChampionShopAPI",
             })
@@ -51,6 +54,4 @@ else
             print(i,v)
         end
     end)
- 
-    
 end
