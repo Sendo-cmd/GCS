a8e37ca5c1294148469f1ac502ffa85576c97023
Sendo_cmd <champandbank1@gmail.com>
2025-01-04 16:50:58 +0700

    a

--- AA_Gem.lua
+++ AA_Gem.lua
@@ -48,4 +48,4 @@ getgenv().Config = {
 	}
 }
 getgenv().Key = "k517c79e9160307a9b87210d"
-repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(4)until Joebiden
+repeat wait(4)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(4)until Joebiden
