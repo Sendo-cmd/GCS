237ad3001a26a04b1f745b79e0474005f85d4dac
Sendo_cmd <champandbank1@gmail.com>
2025-01-05 04:51:13 +0700

    a

--- Games.lua
+++ Games.lua
@@ -800,6 +800,6 @@ for i,v in pairs(ScriptLists) do
             Loaded = true
         end)
 
-        repeat task.wait() until Loaded or (Timer - tick()) > 2
+        repeat task.wait() until Loaded or (Timer - tick()) > 4
     end
 end
