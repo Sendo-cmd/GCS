94b2521ff4890617526aeaba6d13f28307754d89
Sendo_cmd <champandbank1@gmail.com>
2025-01-06 00:13:30 +0700

    a

--- AA_System.lua
+++ AA_System.lua
@@ -330,6 +330,20 @@ _G.User = {
         ["Select Level"] = "1", 
         ["Hard"] = false,
     },
+    ["Etaa_xm"] = {
+        ["Select Mode"] = "Event", 
+    
+        ["Select Map"] = "Frozen Abyss", 
+        ["Select Level"] = "1", 
+        ["Hard"] = false,
+    },
+    ["supakitdunk"] = {
+        ["Select Mode"] = "Infinite", -- Raid , Legend Stage , Infinite
+    
+        ["Select Map"] = "Planet Greenie",
+        ["Select Level"] = 1, -- Story & Legend Stage & Raid
+        ["Hard"] = false, -- Story
+    },
 }
 repeat task.wait(40) until game:IsLoaded()
 
--- Games.lua
+++ Games.lua
@@ -121,6 +121,8 @@ Accounts = {
     },
     ["supakitdunk"] = {
         "Log",
+        "Marco",
+        "FarmGem",
         "System",
     },
     ["didbdknend"] = {
