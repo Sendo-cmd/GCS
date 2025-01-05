4ab9a24ba4207e1b69ef369c0c58e3ace9e19be4
Sendo_cmd <champandbank1@gmail.com>
2025-01-05 18:40:18 +0700

    a

--- AV_DungBase.lua
+++ AV_DungBase.lua
@@ -122,5 +122,5 @@ getgenv().Config = {
 		}
 	}
 }
-getgenv().Key = "k517c79e9160307a9b87210d"
+getgenv().Key = "kFreePremiumKey2025x8Jan"
 repeat wait(6)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(2)until Joebiden
