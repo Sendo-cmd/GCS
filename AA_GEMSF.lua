6a7ebcaf866ea9c59b14572e25acb03bcd9c74b3
Sendo_cmd <champandbank1@gmail.com>
2025-01-17 06:34:33 +0700

    a

--- AA_GEMSF.lua
+++ AA_GEMSF.lua
@@ -1,32 +1,188 @@
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().EquipMacroUnit = true
 getgenv().Config = {
-	["AutoSave"] = true,
+	["Summoner"] = {
+		["Exchange Legacy Gem"] = 0
+	},
 	["Game Finished"] = {
 		["Auto Return Lobby"] = true,
 		["Auto Replay"] = true
 	},
-	["Joiner Cooldown"] = 20,
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
-		["Enable"] = true,
-		["Ignore Upgrade"] = {
-			["6"] = true
+		["Placement Type"] = {
+			["Otherwordly Plane"] = "Middle Position",
+			["Frozen Abyss"] = "Middle Position",
+			["Strange Town"] = "Middle Position",
+			["Fiend City"] = "Middle Position",
+			["Magic Hills"] = "Middle Position",
+			["Sky Aircraft"] = "Middle Position",
+			["Snowy Town"] = "Middle Position",
+			["Alien Spaceship (Underwater)"] = "Middle Position",
+			["Fabled Kingdom"] = "Middle Position",
+			["Ruined City"] = "Middle Position",
+			["Nightmare Train"] = "Middle Position",
+			["Alien Spaceship (Final)"] = "Middle Position",
+			["Strange Town (Haunted)"] = "Middle Position",
+			["Ruined City (Midnight)"] = "Middle Position",
+			["Haunted Academy"] = "Middle Position",
+			["Anniversary Island"] = "Middle Position",
+			["Fabled Kingdom (Cube)"] = "Middle Position",
+			["Puppet Island (Threads)"] = "Middle Position",
+			["Magic Town"] = "Middle Position",
+			["Haunted Academy (Summer)"] = "Middle Position",
+			["Alien Spaceship"] = "Middle Position",
+			["Storm Hideout"] = "Middle Position",
+			["Walled City"] = "Middle Position",
+			["Planet Greenie (Haunted)"] = "Middle Position",
+			["Navy Bay"] = "Middle Position",
+			["Snowy Kingdom"] = "Middle Position",
+			["Virtual Dungeon (Bosses)"] = "Middle Position",
+			["Sand Village"] = "Middle Position",
+			["Ant Kingdom (Summer)"] = "Middle Position",
+			["Rain Village"] = "Middle Position",
+			["Fabled Kingdom (Summer)"] = "Middle Position",
+			["Planet Greenie (Frozen)"] = "Middle Position",
+			["Spirit World"] = "Middle Position",
+			["Magic Town (Snow)"] = "Middle Position",
+			["Sacred Planet"] = "Middle Position",
+			["Walled City (Midnight)"] = "Middle Position",
+			["Space Center (New Moon)"] = "Middle Position",
+			["Storm Hideout (Final)"] = "Middle Position",
+			["Haunted Mansion"] = "Middle Position",
+			["Haunted Academy (Frozen)"] = "Middle Position",
+			["Navy Bay (Midnight)"] = "Middle Position",
+			["Sand Village (Snow)"] = "Middle Position",
+			["Future City"] = "Middle Position",
+			["Spirit Invasion"] = "Middle Position",
+			["Future City (Tyrant's Invasion)"] = "Middle Position",
+			["Frozen Abyss (Six Realms)"] = "Middle Position",
+			["Hellish City"] = "Middle Position",
+			["Virtual Dungeon"] = "Middle Position",
+			["Fabled Kingdom (Generals)"] = "Middle Position",
+			["Magic Hills (Demonic)"] = "Middle Position",
+			["Space Center"] = "Middle Position",
+			["Mountain Temple"] = "Middle Position",
+			["Magic Hills (Elf Invasion)"] = "Middle Position",
+			["Wish City"] = "Middle Position",
+			["Ruined City (The Menace)"] = "Middle Position",
+			["Puppet Island"] = "Middle Position",
+			["Fiend City (Winter)"] = "Middle Position",
+			["Dungeon Throne"] = "Middle Position",
+			["Magic Town (Haunted)"] = "Middle Position",
+			["Puppet Island (Summer)"] = "Middle Position",
+			["Cursed Festival"] = "Middle Position",
+			["Power Contest"] = "Middle Position",
+			["Walled City (Summer)"] = "Middle Position",
+			["Ant Kingdom"] = "Middle Position",
+			["Planet Greenie (Summer)"] = "Middle Position",
+			["Walled City (Winter)"] = "Middle Position",
+			["Spirit Town"] = "Middle Position",
+			["Planet Greenie"] = "Middle Position"
 		},
+		["Enable"] = true,
+		["Auto Fuse Vego-Carrot Anchor"] = "Carrot",
 		["Upgrade Method"] = "Lowest Level (Spread Upgrade)",
-		["Focus on Farm"] = true,
 		["Middle Position"] = {
 			["Virtual Dungeon"] = "103.89673614501953, 37.418487548828125, 10.653985977172852"
-		}
-	},
-	["Macros"] = {
-		["Macro Retry Limit"] = 0,
-		["No Ignore Sell Timing"] = true,
-		["Ignore Macro Timing"] = true
+		},
+		["Upgrade Cap"] = {
+			["1"] = 0,
+			["3"] = 0,
+			["2"] = 0,
+			["5"] = 0,
+			["4"] = 0,
+			["6"] = -1
+		},
+		["Distance Threshold"] = {
+			["Otherwordly Plane"] = 15,
+			["Frozen Abyss"] = 15,
+			["Strange Town"] = 15,
+			["Fiend City"] = 15,
+			["Magic Hills"] = 15,
+			["Sky Aircraft"] = 15,
+			["Snowy Town"] = 15,
+			["Alien Spaceship (Underwater)"] = 15,
+			["Fabled Kingdom"] = 15,
+			["Ruined City"] = 15,
+			["Nightmare Train"] = 15,
+			["Alien Spaceship (Final)"] = 15,
+			["Strange Town (Haunted)"] = 15,
+			["Ruined City (Midnight)"] = 15,
+			["Haunted Academy"] = 15,
+			["Anniversary Island"] = 15,
+			["Fabled Kingdom (Cube)"] = 15,
+			["Puppet Island (Threads)"] = 15,
+			["Magic Town"] = 15,
+			["Haunted Academy (Summer)"] = 15,
+			["Alien Spaceship"] = 15,
+			["Storm Hideout"] = 15,
+			["Walled City"] = 15,
+			["Planet Greenie (Haunted)"] = 15,
+			["Navy Bay"] = 15,
+			["Snowy Kingdom"] = 15,
+			["Virtual Dungeon (Bosses)"] = 15,
+			["Sand Village"] = 15,
+			["Ant Kingdom (Summer)"] = 15,
+			["Rain Village"] = 15,
+			["Fabled Kingdom (Summer)"] = 15,
+			["Planet Greenie (Frozen)"] = 15,
+			["Spirit World"] = 15,
+			["Magic Town (Snow)"] = 15,
+			["Sacred Planet"] = 15,
+			["Walled City (Midnight)"] = 15,
+			["Space Center (New Moon)"] = 15,
+			["Storm Hideout (Final)"] = 15,
+			["Haunted Mansion"] = 15,
+			["Haunted Academy (Frozen)"] = 15,
+			["Navy Bay (Midnight)"] = 15,
+			["Sand Village (Snow)"] = 15,
+			["Future City"] = 15,
+			["Spirit Invasion"] = 15,
+			["Future City (Tyrant's Invasion)"] = 15,
+			["Frozen Abyss (Six Realms)"] = 15,
+			["Hellish City"] = 15,
+			["Virtual Dungeon"] = 15,
+			["Fabled Kingdom (Generals)"] = 15,
+			["Magic Hills (Demonic)"] = 15,
+			["Space Center"] = 15,
+			["Mountain Temple"] = 15,
+			["Magic Hills (Elf Invasion)"] = 15,
+			["Wish City"] = 15,
+			["Ruined City (The Menace)"] = 15,
+			["Puppet Island"] = 15,
+			["Fiend City (Winter)"] = 15,
+			["Dungeon Throne"] = 15,
+			["Magic Town (Haunted)"] = 15,
+			["Puppet Island (Summer)"] = 15,
+			["Cursed Festival"] = 15,
+			["Power Contest"] = 15,
+			["Walled City (Summer)"] = 15,
+			["Ant Kingdom"] = 15,
+			["Planet Greenie (Summer)"] = 15,
+			["Walled City (Winter)"] = 15,
+			["Spirit Town"] = 15,
+			["Planet Greenie"] = 15
+		},
+		["Focus on Farm"] = true
 	},
 	["Claimer"] = {
 		["Auto Claim Quest"] = true,
 		["Auto Claim Present"] = true
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true
+	},
+	["Macros"] = {
+		["No Ignore Sell Timing"] = true,
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true
 	},
 	["Secure"] = {
 		["Walk Around"] = true
@@ -66,25 +222,18 @@ getgenv().Config = {
 			["Planet Greenie"] = true
 		}
 	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true
-	},
+	["AutoSave"] = true,
 	["World Joiner"] = {
 		["Act"] = "Infinite",
 		["Enable"] = true,
 		["World"] = "Planet Greenie"
 	},
 	["Gameplay"] = {
-		["Auto Sell"] = {
-			["Wave"] = 1
-		},
 		["Smart Auto Ability"] = {
 			["Wind Dragon"] = true,
-			["Supreme Being (Sovereign)"] = true,
 			["Time Wizard (Chronos)"] = true,
 			["Lulu (Emperor)"] = true,
+			["Supreme Being (Sovereign)"] = true,
 			["Commander"] = true,
 			["Delinquent"] = true,
 			["Usurper (Founder)"] = true,
@@ -97,17 +246,21 @@ getgenv().Config = {
 			["Menace (Terror)"] = true,
 			["Priest (Heaven)"] = true
 		},
+		["Auto Leave"] = {
+			["Wave"] = 1
+		},
+		["Auto Vote Start"] = true,
 		["Auto Skip Wave"] = true,
 		["Auto Sell Farm"] = {
 			["Enable"] = true,
 			["Wave"] = 15
 		},
-		["Auto Vote Start"] = true
+		["Auto Sell"] = {
+			["Enable"] = true,
+			["Wave"] = 25
+		}
 	},
-	["Failsafe"] = {
-		["Auto Rejoin"] = true,
-		["Teleport Lobby if Player"] = true
-	}
+	["Joiner Cooldown"] = 20
 }
 getgenv().Key = "k7d27caec454d21cbd95104d"
 repeat wait(2)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(7)until Joebiden
