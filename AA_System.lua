8e4c45aac66df7cfd241b36f6a02ea7145e8daa5
Sendo_cmd <champandbank1@gmail.com>
2025-01-17 18:14:40 +0700

    a

--- AA_System.lua
+++ AA_System.lua
@@ -527,9 +527,9 @@ _G.User = {
         ["Hard"] = false, -- Story
     },
     ["Vana_Ungkap"] = {
-        ["Select Mode"] = "Infinite", -- Raid , Legend Stage , Infinite
+        ["Select Mode"] = "Event", -- Raid , Legend Stage , Infinite
     
-        ["Select Map"] = "Planet Greenie",
+        ["Select Map"] = "Frozen Abyss",
         ["Select Level"] = 1, -- Story & Legend Stage & Raid
         ["Hard"] = false, -- Story
     },
