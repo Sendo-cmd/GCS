5b1df85fcc20b59c3e33b8673c2cf5b46ec5fff8
Sendo_cmd <champandbank1@gmail.com>
2025-01-19 14:49:18 +0700

    a

--- AA_HsND.lua
+++ AA_HsND.lua
@@ -10,7 +10,7 @@ getgenv().Config = {
 	["Performance"] = {
 		["Delete Map"] = true,
 		["Boost FPS"] = true,
-		["Black Screen"] = true
+		["Black Screen"] = false
 	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
