d2c4d8edf9a1f6d62723f533cb95656f268aef4b
Sendo_cmd <champandbank1@gmail.com>
2025-01-19 09:33:08 +0700

    a

--- AA_HsND.lua
+++ AA_HsND.lua
@@ -172,7 +172,7 @@ getgenv().Config = {
 			["Planet Greenie"] = 15
 		},
 		["Place Gap"] = {
-			["Frozen Abyss"] = 1.3
+			["Frozen Abyss"] = 1.4
 		}
 	},
 	["Holiday Hunt Joiner"] = {
--- AA_StarRem.lua
+++ AA_StarRem.lua
@@ -173,7 +173,7 @@ getgenv().Config = {
 			["Planet Greenie"] = 15
 		},
 		["Place Gap"] = {
-			["Frozen Abyss"] = 1.3
+			["Frozen Abyss"] = 1.4
 		}
 	},
 	["Raid Joiner"] = {
