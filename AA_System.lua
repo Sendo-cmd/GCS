477845360e6001968d276787fa18eef671c2290a
Sendo_cmd <champandbank1@gmail.com>
2025-02-19 06:50:15 +0700

    a

--- AA_System.lua
+++ AA_System.lua
@@ -1929,7 +1929,7 @@ if Settings["Challenge"] then
 
     
 end
-print("dont give a shit bout challenge")
+
 if Settings["Evo"] and game.PlaceId == 8304191830  then
     -- can craft > normal > star
     local Units = require(game:GetService("ReplicatedStorage").src.Data.Units)
