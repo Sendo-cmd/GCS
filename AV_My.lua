3dcfa093cbb1cb624427f7da8cce8c83106e3cae
Sendo_cmd <champandbank1@gmail.com>
2025-02-22 06:07:13 +0700

    a

--- AV_My.lua
+++ AV_My.lua
@@ -2,23 +2,14 @@ setfpscap(7)
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().RedeemAllCode = true
 getgenv().Config = {
-	["AutoSave"] = true,
-	["Summoner"] = {
-		["Teleport Lobby new Banner"] = true,
-		["Unselect if Summoned"] = true,
-		["Delete Rarity"] = {
-			["Epic"] = true,
-			["Legendary"] = true,
-			["Rare"] = true
-		},
-		["Special Unit"] = {
-			["Saber"] = true
-		},
-		["Auto Summon Special"] = true
+	["Macros"] = {
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
 	},
 	["Portal Joiner"] = {
-		["Auto Join"] = true,
 		["Tier Cap"] = 10,
+		["Auto Join"] = true,
 		["Portal Reward Picker"] = {
 			["Enable"] = true,
 			["Prioritize"] = {
@@ -28,47 +19,32 @@ getgenv().Config = {
 			}
 		}
 	},
-	["Macros"] = {
-		["Macro Retry Limit"] = 0,
-		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true
-	},
-	["Claimer"] = {
-		["Auto Claim Quest"] = true,
-		["Auto Claim Milestone"] = true,
-		["Auto Claim Achievement"] = true,
-		["Auto Claim Daily Reward"] = true,
-		["Auto Claim Collection"] = true,
-		["Auto Claim Enemy Index"] = true,
-		["Auto Claim Collection Milestone"] = true
-	},
-	["Match Finished"] = {
-		["Auto Next"] = true
-	},
-	["Failsafe"] = {
-		["Auto Rejoin"] = true
-	},
-	["Secure"] = {
-		["Walk Around"] = true
-	},
-	["AutoExecute"] = true,
-	["Gameplay"] = {
-		["Saber Event"] = {
-			["Servant"] = "Berserker"
-		},
-		["Auto Sell Farm"] = {
-			["Enable"] = true,
-			["Wave"] = 20
-		},
-		["Auto Skip Wave"] = true,
-		["Auto Use Ability"] = true
-	},
 	["Performance"] = {
 		["Delete Map"] = true,
 		["Boost FPS"] = true,
 		["Black Screen"] = true,
 		["Delete Entities"] = true
 	},
+	["AutoExecute"] = true,
+	["Match Finished"] = {
+		["Auto Next"] = true
+	},
+	["Summoner"] = {
+		["Auto Summon Special"] = true,
+		["Unselect if Summoned"] = true,
+		["Delete Rarity"] = {
+			["Epic"] = true,
+			["Legendary"] = true,
+			["Rare"] = true
+		},
+		["Special Unit"] = {
+			["Saber"] = true
+		},
+		["Teleport Lobby new Banner"] = true,
+		["Insanely Fast Open"] = true,
+		["Auto Open Gift Box"] = true
+	},
+	["AutoSave"] = true,
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
 		["Place Cap"] = {
@@ -98,13 +74,39 @@ getgenv().Config = {
 			["2"] = 0,
 			["5"] = 0,
 			["4"] = 0,
-			["6"] = 0
+			["6"] = -1
 		},
-		["Focus on Farm"] = true,
 		["Middle Position"] = {
 			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781"
-		}
+		},
+		["Focus on Farm"] = true
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Collection Milestone"] = true
+	},
+	["Gameplay"] = {
+		["Saber Event"] = {
+			["Servant"] = "Berserker"
+		},
+		["Auto Use Ability"] = true,
+		["Auto Sell Farm"] = {
+			["Enable"] = true,
+			["Wave"] = 20
+		},
+		["Auto Skip Wave"] = true
 	}
 }
 getgenv().Key = "sZoENIPLYqhElsjhrFywrbFsAGeUzAdm"
