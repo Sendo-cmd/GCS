103ef7e8beec3b7c24475c688321b4b4bdc0192e
Sendo_cmd <champandbank1@gmail.com>
2025-02-26 05:05:22 +0700

    a

--- AV_My2.lua
+++ AV_My2.lua
@@ -17,67 +17,6 @@ getgenv().Config = {
 		["Teleport Lobby new Banner"] = true,
 		["Auto Open Gift Box"] = true
 	},
-	["Macros"] = {
-		["Macro Retry Limit"] = 0,
-		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true
-	},
-	["Stage Joiner"] = {
-		["Auto Join"] = true,
-		["Stage"] = "Spirit Society",
-		["Act"] = "Infinite"
-	},
-	["Portal Joiner"] = {
-		["Tier Cap"] = 10,
-		["Portal Reward Picker"] = {
-			["Enable"] = true,
-			["Prioritize"] = {
-				["Shibuya Aftermath"] = 2,
-				["Spider Forest"] = 1,
-				["Planet Namak"] = 3
-			}
-		}
-	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["Claimer"] = {
-		["Auto Claim Quest"] = true,
-		["Auto Claim Milestone"] = true,
-		["Auto Claim Daily Reward"] = true,
-		["Auto Claim Achievement"] = true,
-		["Auto Claim Collection"] = true,
-		["Auto Claim Enemy Index"] = true,
-		["Auto Claim Collection Milestone"] = true
-	},
-	["Match Finished"] = {
-		["Auto Replay"] = true,
-		["Auto Next"] = true,
-		["Replay Amount"] = 0
-	},
-	["AutoSave"] = true,
-	["Failsafe"] = {
-		["Auto Rejoin"] = true
-	},
-	["Secure"] = {
-		["Walk Around"] = true
-	},
-	["AutoExecute"] = true,
-	["Gameplay"] = {
-		["Saber Event"] = {
-			["Servant"] = "Berserker"
-		},
-		["Auto Sell Farm"] = {
-			["Enable"] = true,
-			["Wave"] = 15
-		},
-		["Auto Vote Start"] = true,
-		["Auto Use Ability"] = true,
-		["Auto Skip Wave"] = true
-	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
 		["Enable"] = true,
@@ -103,13 +42,77 @@ getgenv().Config = {
 			["4"] = 0,
 			["6"] = -1
 		},
+		["Focus on Farm"] = true,
 		["Middle Position"] = {
-			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
 			["Golden Castle"] = "-100.51405334472656, -0.16030120849609375, -210.62820434570312",
+			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
 			["Spider Forest"] = "-324.47222900390625, 1644.5369873046875, -319.5542297363281",
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781"
+		}
+	},
+	["Stage Joiner"] = {
+		["Auto Join"] = true,
+		["Act"] = "Infinite",
+		["Stage"] = "Spirit Society"
+	},
+	["Portal Joiner"] = {
+		["Tier Cap"] = 10,
+		["Portal Reward Picker"] = {
+			["Enable"] = true,
+			["Prioritize"] = {
+				["Shibuya Aftermath"] = 2,
+				["Spider Forest"] = 1,
+				["Planet Namak"] = 3
+			}
+		}
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Quest"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Collection Milestone"] = true
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true,
+		["Auto Next"] = true,
+		["Replay Amount"] = 0
+	},
+	["AutoSave"] = true,
+	["Failsafe"] = {
+		["Auto Rejoin"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["AutoExecute"] = true,
+	["Gameplay"] = {
+		["Auto Sell"] = {
+			["Enable"] = true,
+			["Wave"] = 15
 		},
-		["Focus on Farm"] = true
+		["Saber Event"] = {
+			["Servant"] = "Berserker"
+		},
+		["Auto Sell Farm"] = {
+			["Wave"] = 15
+		},
+		["Auto Vote Start"] = true,
+		["Auto Use Ability"] = true,
+		["Auto Skip Wave"] = true
+	},
+	["Macros"] = {
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
 	}
 }
 getgenv().Key = "sZoENIPLYqhElsjhrFywrbFsAGeUzAdm"
