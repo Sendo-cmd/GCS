198ef00eddfc9cb23c4c692cc735f72385ced086
Sendo_cmd <champandbank1@gmail.com>
2025-02-25 14:45:10 +0700

    a

--- AA_System.lua
+++ AA_System.lua
@@ -1194,7 +1194,6 @@ _G.User = {
         ["Select Level"] = "3", 
         ["Hard"] = false,
         ["Daily"] = false,
-        ["Challenge"] = false,
         ["Party Mode"] = true,
         ["Party Member"] = {
             "CGFONE06m",
