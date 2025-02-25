e750edf9a1dc372511b3d731ba4561009be07e6f
Sendo_cmd <champandbank1@gmail.com>
2025-02-25 13:55:35 +0700

    a

--- AA_System.lua
+++ AA_System.lua
@@ -1191,7 +1191,7 @@ _G.User = {
         ["Select Mode"] = "Raid", 
     
         ["Select Map"] = "Sacred Planet", 
-        ["Select Level"] = "4", 
+        ["Select Level"] = "3", 
         ["Hard"] = false,
         ["Daily"] = false,
         ["Party Mode"] = true,
