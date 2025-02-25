2c9d479ade9edbf2664d19256653d8f2bb030623
Sendo_cmd <champandbank1@gmail.com>
2025-02-26 04:54:58 +0700

    a

--- AV_My2.lua
+++ AV_My2.lua
@@ -2,16 +2,84 @@ setfpscap(7)
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().RedeemAllCode = true
 getgenv().Config = {
+	["Summoner"] = {
+		["Auto Summon Special"] = true,
+		["Unselect if Summoned"] = true,
+		["Special Unit"] = {
+			["Eizan"] = true,
+			["Oryo"] = true
+		},
+		["Delete Rarity"] = {
+			["Epic"] = true,
+			["Legendary"] = true,
+			["Rare"] = true
+		},
+		["Teleport Lobby new Banner"] = true,
+		["Auto Open Gift Box"] = true
+	},
+	["Macros"] = {
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	},
+	["Stage Joiner"] = {
+		["Auto Join"] = true,
+		["Stage"] = "Spirit Society",
+		["Act"] = "Infinite"
+	},
+	["Portal Joiner"] = {
+		["Tier Cap"] = 10,
+		["Portal Reward Picker"] = {
+			["Enable"] = true,
+			["Prioritize"] = {
+				["Shibuya Aftermath"] = 2,
+				["Spider Forest"] = 1,
+				["Planet Namak"] = 3
+			}
+		}
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Quest"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Collection Milestone"] = true
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true,
+		["Auto Next"] = true,
+		["Replay Amount"] = 0
+	},
+	["AutoSave"] = true,
+	["Failsafe"] = {
+		["Auto Rejoin"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["AutoExecute"] = true,
+	["Gameplay"] = {
+		["Saber Event"] = {
+			["Servant"] = "Berserker"
+		},
+		["Auto Sell Farm"] = {
+			["Enable"] = true,
+			["Wave"] = 15
+		},
+		["Auto Vote Start"] = true,
+		["Auto Use Ability"] = true,
+		["Auto Skip Wave"] = true
+	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
-		["Place Cap"] = {
-			["1"] = 0,
-			["3"] = 0,
-			["2"] = 0,
-			["5"] = 0,
-			["4"] = 0,
-			["6"] = 0
-		},
 		["Enable"] = true,
 		["Upgrade Method"] = "Hotbar left to right (until Max)",
 		["Prefer Position"] = {
@@ -35,87 +103,13 @@ getgenv().Config = {
 			["4"] = 0,
 			["6"] = -1
 		},
-		["Focus on Farm"] = true,
 		["Middle Position"] = {
-			["Golden Castle"] = "-100.51405334472656, -0.16030120849609375, -210.62820434570312",
 			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
+			["Golden Castle"] = "-100.51405334472656, -0.16030120849609375, -210.62820434570312",
 			["Spider Forest"] = "-324.47222900390625, 1644.5369873046875, -319.5542297363281",
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781"
-		}
-	},
-	["Worldline Joiner"] = {
-		["Auto Join"] = true
-	},
-	["Portal Joiner"] = {
-		["Tier Cap"] = 10,
-		["Portal Reward Picker"] = {
-			["Enable"] = true,
-			["Prioritize"] = {
-				["Shibuya Aftermath"] = 2,
-				["Spider Forest"] = 1,
-				["Planet Namak"] = 3
-			}
-		}
-	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["Claimer"] = {
-		["Auto Claim Quest"] = true,
-		["Auto Claim Milestone"] = true,
-		["Auto Claim Achievement"] = true,
-		["Auto Claim Daily Reward"] = true,
-		["Auto Claim Collection"] = true,
-		["Auto Claim Enemy Index"] = true,
-		["Auto Claim Collection Milestone"] = true
-	},
-	["Match Finished"] = {
-		["Auto Replay"] = true,
-		["Auto Next"] = true,
-		["Replay Amount"] = 0
-	},
-	["Summoner"] = {
-		["Auto Summon Special"] = true,
-		["Unselect if Summoned"] = true,
-		["Special Unit"] = {
-			["Eizan"] = true,
-			["Oryo"] = true
 		},
-		["Delete Rarity"] = {
-			["Epic"] = true,
-			["Legendary"] = true,
-			["Rare"] = true
-		},
-		["Teleport Lobby new Banner"] = true,
-		["Auto Open Gift Box"] = true
-	},
-	["AutoSave"] = true,
-	["Failsafe"] = {
-		["Auto Rejoin"] = true
-	},
-	["Secure"] = {
-		["Walk Around"] = true
-	},
-	["AutoExecute"] = true,
-	["Gameplay"] = {
-		["Saber Event"] = {
-			["Servant"] = "Berserker"
-		},
-		["Auto Use Ability"] = true,
-		["Auto Vote Start"] = true,
-		["Auto Sell Farm"] = {
-			["Enable"] = true,
-			["Wave"] = 15
-		},
-		["Auto Skip Wave"] = true
-	},
-	["Macros"] = {
-		["Macro Retry Limit"] = 0,
-		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true
+		["Focus on Farm"] = true
 	}
 }
 getgenv().Key = "sZoENIPLYqhElsjhrFywrbFsAGeUzAdm"
