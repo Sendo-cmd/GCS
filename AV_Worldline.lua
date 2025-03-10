99c5ff2a7beeda741e9dcfddb2f571b4d561270b
Sendo_cmd <champandbank1@gmail.com>
2025-03-10 08:21:44 +0700

    a

--- AV_Worldline.lua
+++ AV_Worldline.lua
@@ -1,23 +1,35 @@
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().Config = {
-	["AutoSave"] = true,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
 	["Worldline Joiner"] = {
 		["Auto Join"] = true
 	},
+	["AutoSave"] = true,
 	["Macros"] = {
+		["Macro"] = "GCW",
 		["Ignore Macro Timing"] = true,
 		["Macro Retry Limit"] = 0,
+		["Play"] = true,
 		["Worldline Macro"] = {
-			["Golden Castle"] = "GCW",
+			["Shibuya Aftermath"] = "SAW",
 			["Double Dungeon"] = "DDW",
 			["Planet Namak"] = "NW",
-			["Underground Church"] = "UCW",
 			["Shibuya Station"] = "SSW",
-			["Shibuya Aftermath"] = "SAW",
-			["Kuinshi Palace"] = "KPW"
+			["Underground Church"] = "UCW",
+			["Kuinshi Palace"] = "KPW",
+			["Golden Castle"] = "GCW"
 		},
-		["No Ignore Sell Timing"] = true,
-		["Play"] = true
+		["No Ignore Sell Timing"] = true
+	},
+	["Match Finished"] = {
+		["Auto Replay"] = true,
+		["Auto Next"] = true,
+		["Replay Amount"] = 0
 	},
 	["Love Portal Joiner"] = {
 		["Tier Cap"] = 10,
@@ -32,6 +44,13 @@ getgenv().Config = {
 			}
 		}
 	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Teleport Lobby if Player"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
 	["Winter Portal Joiner"] = {
 		["Tier Cap"] = 10,
 		["Portal Reward Picker"] = {
@@ -42,22 +61,14 @@ getgenv().Config = {
 			}
 		}
 	},
+	["AutoExecute"] = true,
 	["Gameplay"] = {
-		["Saber Event"] = {
-			["Servant"] = "Berserker"
-		},
-		["Auto Sell Farm"] = {
-			["Wave"] = 1
-		},
-		["Auto Skip Wave"] = true,
-		["Auto Use Ability"] = true,
 		["Auto Modifier"] = {
-			["Enable"] = true,
 			["Prioritize"] = {
 				["Strong"] = 3,
 				["Thrice"] = 4,
 				["Warding off Evil"] = 24,
-				["Precise Attack"] = 13,
+				["Champions"] = 12,
 				["Fast"] = 1,
 				["Revitalize"] = 6,
 				["Exploding"] = 2,
@@ -66,27 +77,28 @@ getgenv().Config = {
 				["Fisticuffs"] = 25,
 				["Harvest"] = 17,
 				["Slayer"] = 16,
-				["Champions"] = 12,
+				["Precise Attack"] = 13,
 				["Planning Ahead"] = 15,
-				["Drowsy"] = 8,
+				["No Trait No Problem"] = 23,
 				["Cooldown"] = 19,
 				["Quake"] = 9,
-				["Regen"] = 7,
+				["King's Burden"] = 27,
 				["Range"] = 18,
 				["Common Loot"] = 21,
 				["Damage"] = 20,
-				["King's Burden"] = 27,
+				["Regen"] = 7,
 				["Press It"] = 14,
-				["No Trait No Problem"] = 23,
+				["Drowsy"] = 8,
 				["Shielded"] = 5,
 				["Uncommon Loot"] = 22,
 				["Money Surge"] = 26
 			},
+			["Enable"] = true,
 			["Amount"] = {
 				["Strong"] = 0,
 				["Thrice"] = 0,
 				["Warding off Evil"] = 0,
-				["Precise Attack"] = 0,
+				["Champions"] = 0,
 				["Fast"] = 0,
 				["Revitalize"] = 0,
 				["Exploding"] = 0,
@@ -95,23 +107,32 @@ getgenv().Config = {
 				["Fisticuffs"] = 0,
 				["Harvest"] = 0,
 				["Slayer"] = 0,
-				["Champions"] = 0,
+				["Precise Attack"] = 0,
 				["Planning Ahead"] = 0,
-				["Drowsy"] = 0,
+				["No Trait No Problem"] = 0,
 				["Cooldown"] = 0,
 				["Quake"] = 0,
-				["Regen"] = 0,
+				["King's Burden"] = 0,
 				["Range"] = 0,
 				["Common Loot"] = 0,
 				["Damage"] = 0,
-				["King's Burden"] = 0,
+				["Regen"] = 0,
 				["Press It"] = 0,
-				["No Trait No Problem"] = 0,
+				["Drowsy"] = 0,
 				["Shielded"] = 0,
 				["Uncommon Loot"] = 0,
 				["Money Surge"] = 0
 			}
-		}
+		},
+		["Saber Event"] = {
+			["Servant"] = "Berserker"
+		},
+		["Auto Sell Farm"] = {
+			["Wave"] = 1
+		},
+		["Auto Vote Start"] = true,
+		["Auto Use Ability"] = true,
+		["Auto Skip Wave"] = true
 	}
 }
 getgenv().Key = "sZoENIPLYqhElsjhrFywrbFsAGeUzAdm"
