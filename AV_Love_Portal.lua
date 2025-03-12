39bb15ae7587c9863d6c46ef565c327667a729da
Sendo_cmd <champandbank1@gmail.com>
2025-03-12 13:59:12 +0700

    a

--- AV_Love_Portal.lua
+++ AV_Love_Portal.lua
@@ -1,5 +1,83 @@
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().Config = {
+	["Macros"] = {
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	},
+	["AutoExecute"] = true,
+	["Stage Joiner"] = {
+		["Act"] = "Infinite",
+		["Stage"] = "Planet Namak"
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["AutoSave"] = true,
+	["Love Portal Joiner"] = {
+		["Tier Cap"] = 10,
+		["Auto Next"] = true,
+		["Portal Reward Picker"] = {
+			["Enable"] = true,
+			["Prioritize"] = {
+				["Double Dungeon"] = 3,
+				["Planet Namak"] = 1,
+				["Spirit Society"] = 6,
+				["Shibuya Station"] = 4,
+				["Underground Church"] = 5,
+				["Sand Village"] = 2
+			}
+		}
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Winter Portal Joiner"] = {
+		["Tier Cap"] = 10,
+		["Portal Reward Picker"] = {
+			["Prioritize"] = {
+				["Shibuya Aftermath"] = 2,
+				["Spider Forest"] = 1,
+				["Planet Namak"] = 3
+			}
+		}
+	},
+	["Claimer"] = {
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Collection Milestone"] = true
+	},
+	["Gameplay"] = {
+		["Auto Mohato"] = {
+			["Enable"] = true,
+			["Upgrade"] = 0,
+			["Unit"] = "Sukono"
+		},
+		["Auto Sell"] = {
+			["Enable"] = true,
+			["Wave"] = 22
+		},
+		["Saber Event"] = {
+			["Servant"] = "Berserker"
+		},
+		["Auto Vote Start"] = true,
+		["Auto Skip Wave"] = true,
+		["Auto Use Ability"] = true,
+		["Auto Sell Farm"] = {
+			["Enable"] = true,
+			["Wave"] = 20
+		}
+	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
 		["Place Cap"] = {
@@ -33,75 +111,12 @@ getgenv().Config = {
 			["4"] = 0,
 			["6"] = -1
 		},
+		["Focus on Farm"] = true,
 		["Middle Position"] = {
 			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
 			["Spider Forest"] = "-324.47222900390625, 1644.5369873046875, -319.5542297363281",
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781"
-		},
-		["Focus on Farm"] = true
-	},
-	["AutoExecute"] = true,
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["AutoSave"] = true,
-	["Love Portal Joiner"] = {
-		["Tier Cap"] = 10,
-		["Auto Next"] = true,
-		["Portal Reward Picker"] = {
-			["Enable"] = true,
-			["Prioritize"] = {
-				["Double Dungeon"] = 3,
-				["Planet Namak"] = 1,
-				["Spirit Society"] = 6,
-				["Shibuya Station"] = 4,
-				["Underground Church"] = 5,
-				["Sand Village"] = 2
-			}
 		}
-	},
-	["Macros"] = {
-		["Macro Retry Limit"] = 0,
-		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true
-	},
-	["Failsafe"] = {
-		["Auto Rejoin"] = true
-	},
-	["Secure"] = {
-		["Walk Around"] = true
-	},
-	["Claimer"] = {
-		["Auto Claim Milestone"] = true,
-		["Auto Claim Quest"] = true,
-		["Auto Claim Achievement"] = true,
-		["Auto Claim Daily Reward"] = true,
-		["Auto Claim Collection"] = true,
-		["Auto Claim Enemy Index"] = true,
-		["Auto Claim Collection Milestone"] = true
-	},
-	["Gameplay"] = {
-		["Auto Sell"] = {
-			["Enable"] = true,
-			["Wave"] = 22
-		},
-		["Saber Event"] = {
-			["Servant"] = "Berserker"
-		},
-		["Auto Vote Start"] = true,
-		["Auto Skip Wave"] = true,
-		["Auto Use Ability"] = true,
-		["Auto Sell Farm"] = {
-			["Enable"] = true,
-			["Wave"] = 20
-		}
-	},
-	["Stage Joiner"] = {
-		["Act"] = "Infinite",
-		["Stage"] = "Planet Namak"
 	}
 }
 getgenv().Key = "sZoENIPLYqhElsjhrFywrbFsAGeUzAdm"
