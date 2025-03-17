4a7ad70dca147ef900d4c508bdd39d4dbe058f0a
Sendo_cmd <champandbank1@gmail.com>
2025-03-18 06:22:20 +0700

    a

--- AV_DungAnt.lua
+++ AV_DungAnt.lua
@@ -1,119 +1,49 @@
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().Config = {
 	["AutoSave"] = true,
+	["Dungeon Joiner"] = {
+		["Stage"] = "Ant Island",
+		["Auto Join"] = true,
+		["Act"] = "AntIsland"
+	},
 	["Macros"] = {
 		["Macro Retry Limit"] = 0,
 		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true
+		["No Ignore Sell Timing"] = true,
 	},
-	["Love Portal Joiner"] = {
-		["Tier Cap"] = 10,
-		["Portal Reward Picker"] = {
-			["Prioritize"] = {
-				["Double Dungeon"] = 3,
-				["Planet Namak"] = 1,
-				["Spirit Society"] = 6,
-				["Shibuya Station"] = 4,
-				["Underground Church"] = 5,
-				["Sand Village"] = 2
-			}
-		}
-	},
-	["Winter Portal Joiner"] = {
-		["Tier Cap"] = 10,
-		["Portal Reward Picker"] = {
-			["Prioritize"] = {
-				["Shibuya Aftermath"] = 2,
-				["Spider Forest"] = 1,
-				["Planet Namak"] = 3
-			}
-		}
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Collection Milestone"] = true
 	},
 	["Gameplay"] = {
-		["Occult Hunt"] = {
-			["Restart Unmatch"] = {
-				["Enable"] = true,
-				["Modifier"] = "Money Surge"
-			}
-		},
-		["Ant Island"] = {
-			["Walk To Ant Tunnel"] = true,
-			["Auto Plug Ant Tunnel"] = true
+		["Auto Sell"] = {
+			["Enable"] = false,
+			["Wave"] = 1
 		},
 		["Saber Event"] = {
+			["Auto Select Servant"] = false,
 			["Servant"] = "Berserker"
 		},
-		["Auto Use Ability"] = true,
-		["Auto Vote Start"] = true,
 		["Auto Sell Farm"] = {
 			["Enable"] = true,
 			["Wave"] = 30
 		},
+		["Auto Vote Start"] = true,
+		["Auto Use Ability"] = true,
 		["Auto Skip Wave"] = true,
-		["Auto Modifier"] = {
-			["Enable"] = true,
-			["Prioritize"] = {
-				["Strong"] = 89,
-				["Thrice"] = 0,
-				["Warding off Evil"] = 0,
-				["Precise Attack"] = 0,
-				["Fast"] = 0,
-				["Planning Ahead"] = 0,
-				["Fisticuffs"] = 0,
-				["Exploding"] = 91,
-				["Dodge"] = 0,
-				["Slayer"] = 93,
-				["Immunity"] = 92,
-				["Revitalize"] = 0,
-				["Harvest"] = 99,
-				["Champions"] = 88,
-				["Cooldown"] = 96,
-				["Drowsy"] = 0,
-				["Lifeline"] = 0,
-				["Range"] = 98,
-				["Regen"] = 0,
-				["Exterminator"] = 0,
-				["Damage"] = 97,
-				["Common Loot"] = 94,
-				["King's Burden"] = 0,
-				["No Trait No Problem"] = 0,
-				["Press It"] = 0,
-				["Quake"] = 0,
-				["Shielded"] = 0,
-				["Uncommon Loot"] = 95,
-				["Money Surge"] = 100
-			},
-			["Amount"] = {
-				["Strong"] = 3,
-				["Thrice"] = 3,
-				["Warding off Evil"] = 0,
-				["Precise Attack"] = 0,
-				["Fast"] = 3,
-				["Planning Ahead"] = 0,
-				["Fisticuffs"] = 0,
-				["Exploding"] = 3,
-				["Dodge"] = 3,
-				["Slayer"] = 0,
-				["Immunity"] = 1,
-				["Revitalize"] = 3,
-				["Harvest"] = 0,
-				["Champions"] = 1,
-				["Cooldown"] = 3,
-				["Drowsy"] = 3,
-				["Lifeline"] = 0,
-				["Range"] = 0,
-				["Regen"] = 3,
-				["Exterminator"] = 0,
-				["Damage"] = 0,
-				["Common Loot"] = 0,
-				["King's Burden"] = 0,
-				["No Trait No Problem"] = 0,
-				["Press It"] = 0,
-				["Quake"] = 3,
-				["Shielded"] = 3,
-				["Uncommon Loot"] = 0,
-				["Money Surge"] = 1
-			}
+		["Auto Restart"] = {
+			["Enable"] = false,
+			["Wave"] = 1
+		},
+		["Ant Island"] = {
+			["Walk To Ant Tunnel"] = true,
+			["Auto Plug Ant Tunnel"] = true
 		}
 	},
 	["Misc"] = {
@@ -123,70 +53,76 @@ getgenv().Config = {
 		["Restart Modifier"] = {
 			["Enable"] = true,
 			["Modifier"] = {
+				["No Trait No Problem"] = false,
+				["King's Burden"] = false,
+				["Lifeline"] = false,
+				["Fisticuffs"] = false,
+				["Warding off Evil"] = false,
+				["Exterminator"] = false,
 				["Money Surge"] = true
 			}
 		},
 		["Auto Modifier"] = {
 			["Enable"] = true,
 			["Prioritize"] = {
-				["Strong"] = 89,
+				["Strong"] = 3,
 				["Thrice"] = 0,
 				["Warding off Evil"] = 0,
-				["Precise Attack"] = 0,
+				["Champions"] = 92,
 				["Fast"] = 0,
-				["Planning Ahead"] = 0,
+				["Revitalize"] = 2,
 				["Fisticuffs"] = 0,
-				["Exploding"] = 91,
+				["Exploding"] = 4,
 				["Dodge"] = 0,
-				["Slayer"] = 93,
-				["Immunity"] = 92,
-				["Revitalize"] = 0,
+				["Uncommon Loot"] = 98,
+				["Immunity"] = 91,
+				["Planning Ahead"] = 0,
 				["Harvest"] = 99,
-				["Champions"] = 88,
-				["Cooldown"] = 96,
-				["Drowsy"] = 0,
 				["Lifeline"] = 0,
-				["Range"] = 98,
-				["Regen"] = 0,
-				["Exterminator"] = 0,
-				["Damage"] = 97,
-				["Common Loot"] = 94,
-				["King's Burden"] = 0,
+				["Precise Attack"] = 0,
+				["Drowsy"] = 1,
 				["No Trait No Problem"] = 0,
+				["Cooldown"] = 94,
+				["Exterminator"] = 0,
+				["Regen"] = 0,
+				["Damage"] = 95,
+				["Common Loot"] = 97,
+				["King's Burden"] = 0,
+				["Range"] = 96,
 				["Press It"] = 0,
 				["Quake"] = 0,
 				["Shielded"] = 0,
-				["Uncommon Loot"] = 95,
+				["Slayer"] = 93,
 				["Money Surge"] = 100
 			},
 			["Amount"] = {
 				["Strong"] = 3,
-				["Thrice"] = 3,
+				["Thrice"] = 0,
 				["Warding off Evil"] = 0,
-				["Precise Attack"] = 0,
-				["Fast"] = 3,
-				["Planning Ahead"] = 0,
-				["Fisticuffs"] = 0,
-				["Exploding"] = 3,
-				["Dodge"] = 3,
-				["Slayer"] = 0,
-				["Immunity"] = 1,
-				["Revitalize"] = 3,
-				["Harvest"] = 0,
 				["Champions"] = 1,
-				["Cooldown"] = 3,
-				["Drowsy"] = 3,
-				["Lifeline"] = 0,
+				["Fast"] = 1,
+				["Revitalize"] = 3,
+				["Immunity"] = 1,
+				["Exploding"] = 3,
+				["Dodge"] = 1,
+				["Slayer"] = 0,
+				["Fisticuffs"] = 0,
+				["Planning Ahead"] = 0,
+				["Harvest"] = 0,
+				["Quake"] = 0,
 				["Range"] = 0,
-				["Regen"] = 3,
+				["Lifeline"] = 0,
+				["No Trait No Problem"] = 0,
+				["Regen"] = 0,
+				["King's Burden"] = 0,
 				["Exterminator"] = 0,
 				["Damage"] = 0,
 				["Common Loot"] = 0,
-				["King's Burden"] = 0,
-				["No Trait No Problem"] = 0,
+				["Cooldown"] = 0,
+				["Drowsy"] = 3,
 				["Press It"] = 0,
-				["Quake"] = 3,
-				["Shielded"] = 3,
+				["Precise Attack"] = 0,
+				["Shielded"] = 0,
 				["Uncommon Loot"] = 0,
 				["Money Surge"] = 1
 			}
@@ -199,23 +135,33 @@ getgenv().Config = {
 		["Delete Entities"] = true
 	},
 	["Match Finished"] = {
-		["Auto Replay"] = true,
-		["Replay Amount"] = 0
+		["Auto Replay"] = true
 	},
 	["Failsafe"] = {
 		["Auto Rejoin"] = true
 	},
+	["AutoExecute"] = true,
 	["Secure"] = {
 		["Walk Around"] = true
 	},
-	["AutoExecute"] = true,
-	["Dungeon Joiner"] = {
-		["Stage"] = "Ant Island",
-		["Auto Join"] = true,
-		["Act"] = "AntIsland"
+	["Performance Failsafe"] = {
+		["Ping Freeze"] = false,
+		["Teleport Lobby FPS below"] = {
+			["Enable"] = false,
+			["FPS"] = 5
+		}
 	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
+		["Place and Upgrade"] = false,
+		["Place Cap"] = {
+			["1"] = 0,
+			["3"] = 0,
+			["2"] = 0,
+			["5"] = 0,
+			["4"] = 0,
+			["6"] = 0
+		},
 		["Enable"] = true,
 		["Upgrade Method"] = "Lowest Level (Spread Upgrade)",
 		["Prefer Position"] = {
@@ -228,10 +174,10 @@ getgenv().Config = {
 			["Shibuya Aftermath"] = "Middle",
 			["Mountain Shrine (Natural)"] = "Middle",
 			["Kuinshi Palace"] = "Middle",
-			["Tracks at the Edge of the World"] = "Middle",
+			["Double Dungeon"] = "Middle",
 			["Underground Church"] = "Middle",
 			["Spider Forest"] = "Middle",
-			["Double Dungeon"] = "Middle"
+			["Tracks at the Edge of the World"] = "Middle"
 		},
 		["Upgrade Cap"] = {
 			["1"] = 0,
@@ -243,7 +189,7 @@ getgenv().Config = {
 		},
 		["Focus on Farm"] = true,
 		["Middle Position"] = {
-			["Ant Island"] = "-18.261302947998047, 164.8186492919922, -44.56714630126953"
+			["Ant Island"] = "-16.992002487182617, 164.8186492919922, -44.643760681152344"
 		},
 		["Place Gap"] = {
 			["Golden Castle"] = 2,
@@ -255,10 +201,10 @@ getgenv().Config = {
 			["Shibuya Aftermath"] = 2,
 			["Mountain Shrine (Natural)"] = 2,
 			["Kuinshi Palace"] = 2,
-			["Tracks at the Edge of the World"] = 2,
+			["Double Dungeon"] = 2,
 			["Underground Church"] = 2,
 			["Spider Forest"] = 2,
-			["Double Dungeon"] = 2
+			["Tracks at the Edge of the World"] = 2
 		}
 	}
 }
