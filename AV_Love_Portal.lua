da0981aac01768e3c0e42196c882ef0673697a72
Sendo_cmd <champandbank1@gmail.com>
2025-03-29 21:29:33 +0700

    a

--- AV_Love_Portal.lua
+++ AV_Love_Portal.lua
@@ -13,11 +13,11 @@ getgenv().Config = {
 			["Enable"] = true,
 			["Prioritize"] = {
 				["Double Dungeon"] = 3,
-				["Planet Namak"] = 1,
-				["Spirit Society"] = 6,
-				["Shibuya Station"] = 4,
-				["Underground Church"] = 5,
-				["Sand Village"] = 2
+				["Planet Namak"] = 20,
+				["Spirit Society"] = 17,
+				["Shibuya Station"] = 0,
+				["Underground Church"] = 18,
+				["Sand Village"] = 19
 			}
 		}
 	},
@@ -31,7 +31,15 @@ getgenv().Config = {
 			}
 		}
 	},
-	["AutoExecute"] = true,
+	["Claimer"] = {
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Collection Milestone"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Milestone"] = true
+	},
 	["Gameplay"] = {
 		["Auto Mohato"] = {
 			["Enable"] = true,
@@ -41,12 +49,12 @@ getgenv().Config = {
 		["Saber Event"] = {
 			["Servant"] = "Berserker"
 		},
+		["Auto Use Ability"] = true,
+		["Auto Vote Start"] = true,
 		["Auto Sell Farm"] = {
 			["Enable"] = true,
 			["Wave"] = 20
 		},
-		["Auto Vote Start"] = true,
-		["Auto Use Ability"] = true,
 		["Auto Skip Wave"] = true
 	},
 	["Misc"] = {
@@ -60,25 +68,25 @@ getgenv().Config = {
 				["Warding off Evil"] = 24,
 				["Champions"] = 12,
 				["Fast"] = 1,
-				["Planning Ahead"] = 15,
+				["Revitalize"] = 6,
 				["Fisticuffs"] = 25,
 				["Exploding"] = 2,
 				["Dodge"] = 10,
 				["Uncommon Loot"] = 22,
 				["Immunity"] = 11,
-				["Revitalize"] = 6,
+				["Planning Ahead"] = 15,
 				["Harvest"] = 17,
 				["Precise Attack"] = 13,
 				["Range"] = 18,
-				["Lifeline"] = 29,
 				["No Trait No Problem"] = 23,
+				["Drowsy"] = 8,
 				["Cooldown"] = 19,
 				["Exterminator"] = 28,
-				["Regen"] = 7,
+				["King's Burden"] = 27,
 				["Damage"] = 20,
 				["Common Loot"] = 21,
-				["King's Burden"] = 27,
-				["Drowsy"] = 8,
+				["Regen"] = 7,
+				["Lifeline"] = 29,
 				["Press It"] = 14,
 				["Quake"] = 9,
 				["Shielded"] = 5,
@@ -91,25 +99,25 @@ getgenv().Config = {
 				["Warding off Evil"] = 0,
 				["Champions"] = 0,
 				["Fast"] = 0,
-				["Planning Ahead"] = 0,
+				["Revitalize"] = 0,
 				["Fisticuffs"] = 0,
 				["Exploding"] = 0,
 				["Dodge"] = 0,
 				["Uncommon Loot"] = 0,
 				["Immunity"] = 0,
-				["Revitalize"] = 0,
+				["Planning Ahead"] = 0,
 				["Harvest"] = 0,
 				["Precise Attack"] = 0,
 				["Range"] = 0,
-				["Lifeline"] = 0,
 				["No Trait No Problem"] = 0,
+				["Drowsy"] = 0,
 				["Cooldown"] = 0,
 				["Exterminator"] = 0,
-				["Regen"] = 0,
+				["King's Burden"] = 0,
 				["Damage"] = 0,
 				["Common Loot"] = 0,
-				["King's Burden"] = 0,
-				["Drowsy"] = 0,
+				["Regen"] = 0,
+				["Lifeline"] = 0,
 				["Press It"] = 0,
 				["Quake"] = 0,
 				["Shielded"] = 0,
@@ -118,18 +126,6 @@ getgenv().Config = {
 			}
 		}
 	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["Failsafe"] = {
-		["Auto Rejoin"] = true
-	},
-	["Secure"] = {
-		["Walk Around"] = true
-	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
 		["Enable"] = true,
@@ -137,7 +133,7 @@ getgenv().Config = {
 		["Prefer Position"] = {
 			["Golden Castle"] = "Middle",
 			["Double Dungeon"] = "Middle",
-			["Planet Namak"] = "Middle",
+			["Blood-Red Chamber"] = "Middle",
 			["Ant Island"] = "Middle",
 			["Shibuya Station"] = "Middle",
 			["Tracks at the Edge of the World"] = "Middle",
@@ -149,7 +145,7 @@ getgenv().Config = {
 			["Spirit Society"] = "Middle",
 			["Underground Church"] = "Middle",
 			["Spider Forest"] = "Middle",
-			["Blood-Red Chamber"] = "Middle"
+			["Planet Namak"] = "Middle"
 		},
 		["Upgrade Cap"] = {
 			["1"] = 0,
@@ -159,24 +155,28 @@ getgenv().Config = {
 			["4"] = 0,
 			["6"] = 0
 		},
+		["Focus on Farm"] = true,
 		["Middle Position"] = {
 			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
 			["Spirit Society"] = "201.20594787597656, -0.03500000014901161, 657.1648559570312",
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781",
 			["Underground Church"] = "191.08030700683594, 0.30062153935432434, 112.90398406982422",
 			["Spider Forest"] = "-324.47222900390625, 1644.5369873046875, -319.5542297363281"
-		},
-		["Focus on Farm"] = true
+		}
 	},
-	["Claimer"] = {
-		["Auto Claim Quest"] = true,
-		["Auto Claim Achievement"] = true,
-		["Auto Claim Collection Milestone"] = true,
-		["Auto Claim Daily Reward"] = true,
-		["Auto Claim Collection"] = true,
-		["Auto Claim Enemy Index"] = true,
-		["Auto Claim Milestone"] = true
-	}
+	["Failsafe"] = {
+		["Auto Rejoin"] = true
+	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["AutoExecute"] = true
 }
 getgenv().Key = "k7d27caec454d21cbd95104d"
 repeat wait(4)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
