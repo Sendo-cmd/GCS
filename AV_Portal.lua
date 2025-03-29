9ee34de6d35f18c243cedd31fe3d49dbef1fa8d5
Sendo_cmd <champandbank1@gmail.com>
2025-03-29 23:22:43 +0700

    a

--- AV_Portal.lua
+++ AV_Portal.lua
@@ -1,29 +1,162 @@
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().Config = {
+	["AutoSave"] = true,
+	["Macros"] = {
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	},
+	["Love Portal Joiner"] = {
+		["Tier Cap"] = 10,
+		["Portal Reward Picker"] = {
+			["Prioritize"] = {
+				["Double Dungeon"] = 3,
+				["Planet Namak"] = 1,
+				["Spirit Society"] = 6,
+				["Shibuya Station"] = 4,
+				["Underground Church"] = 5,
+				["Sand Village"] = 2
+			}
+		}
+	},
+	["Winter Portal Joiner"] = {
+		["Buy if out of Portal"] = true,
+		["Auto Next"] = true,
+		["Tier Cap"] = 10,
+		["Portal Reward Picker"] = {
+			["Enable"] = true,
+			["Prioritize"] = {
+				["Shibuya Aftermath"] = 19,
+				["Spider Forest"] = 0,
+				["Planet Namak"] = 20
+			}
+		},
+		["Auto Delete Spider Forest Portal"] = true
+	},
+	["AutoExecute"] = true,
+	["Gameplay"] = {
+		["Saber Event"] = {
+			["Servant"] = "Berserker"
+		},
+		["Auto Vote Start"] = true,
+		["Auto Skip Wave"] = true,
+		["Auto Sell Farm"] = {
+			["Enable"] = true,
+			["Wave"] = 20
+		},
+		["Auto Use Ability"] = true
+	},
+	["Misc"] = {
+		["Redeem Code"] = true
+	},
+	["Modifier"] = {
+		["Auto Modifier"] = {
+			["Prioritize"] = {
+				["Strong"] = 3,
+				["Thrice"] = 4,
+				["Warding off Evil"] = 24,
+				["Champions"] = 12,
+				["Fast"] = 1,
+				["Planning Ahead"] = 15,
+				["Fisticuffs"] = 25,
+				["Exploding"] = 2,
+				["Dodge"] = 10,
+				["Uncommon Loot"] = 22,
+				["Immunity"] = 11,
+				["Revitalize"] = 6,
+				["Harvest"] = 17,
+				["Precise Attack"] = 13,
+				["Range"] = 18,
+				["No Trait No Problem"] = 23,
+				["Drowsy"] = 8,
+				["Cooldown"] = 19,
+				["Exterminator"] = 28,
+				["King's Burden"] = 27,
+				["Damage"] = 20,
+				["Common Loot"] = 21,
+				["Regen"] = 7,
+				["Lifeline"] = 29,
+				["Press It"] = 14,
+				["Quake"] = 9,
+				["Shielded"] = 5,
+				["Slayer"] = 16,
+				["Money Surge"] = 26
+			},
+			["Amount"] = {
+				["Strong"] = 0,
+				["Thrice"] = 0,
+				["Warding off Evil"] = 0,
+				["Champions"] = 0,
+				["Fast"] = 0,
+				["Planning Ahead"] = 0,
+				["Fisticuffs"] = 0,
+				["Exploding"] = 0,
+				["Dodge"] = 0,
+				["Uncommon Loot"] = 0,
+				["Immunity"] = 0,
+				["Revitalize"] = 0,
+				["Harvest"] = 0,
+				["Precise Attack"] = 0,
+				["Range"] = 0,
+				["No Trait No Problem"] = 0,
+				["Drowsy"] = 0,
+				["Cooldown"] = 0,
+				["Exterminator"] = 0,
+				["King's Burden"] = 0,
+				["Damage"] = 0,
+				["Common Loot"] = 0,
+				["Regen"] = 0,
+				["Lifeline"] = 0,
+				["Press It"] = 0,
+				["Quake"] = 0,
+				["Shielded"] = 0,
+				["Slayer"] = 0,
+				["Money Surge"] = 0
+			}
+		}
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true
+	},
+	["Claimer"] = {
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection"] = true,
+		["Auto Claim Enemy Index"] = true,
+		["Auto Claim Collection Milestone"] = true
+	},
+	["Secure"] = {
+		["Random Offset"] = true,
+		["Walk Around"] = true
+	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
-		["Place Cap"] = {
-			["1"] = 0,
-			["3"] = 0,
-			["2"] = 0,
-			["5"] = 0,
-			["4"] = 0,
-			["6"] = 0,
-		},
 		["Enable"] = true,
 		["Upgrade Method"] = "Hotbar left to right (until Max)",
 		["Prefer Position"] = {
 			["Golden Castle"] = "Middle",
-			["Double Dungeon"] = "Middle",
-			["Planet Namak"] = "Middle",
+			["Spirit Society"] = "Middle",
+			["Blood-Red Chamber"] = "Middle",
 			["Tracks at the Edge of the World"] = "Middle",
 			["Shibuya Station"] = "Middle",
+			["Planet Namak"] = "Middle",
 			["Sand Village"] = "Middle",
 			["Shibuya Aftermath"] = "Middle",
+			["Cavern"] = "Middle",
+			["Mountain Shrine (Natural)"] = "Middle",
 			["Kuinshi Palace"] = "Middle",
+			["Ant Island"] = "Middle",
 			["Underground Church"] = "Middle",
 			["Spider Forest"] = "Middle",
-			["Spirit Society"] = "Middle"
+			["Double Dungeon"] = "Middle"
 		},
 		["Upgrade Cap"] = {
 			["1"] = 0,
@@ -39,83 +172,6 @@ getgenv().Config = {
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781"
 		},
 		["Focus on Farm"] = true
-	},
-	["AutoExecute"] = true,
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["AutoSave"] = true,
-	["Love Portal Joiner"] = {
-		["Tier Cap"] = 10,
-		["Portal Reward Picker"] = {
-			["Prioritize"] = {
-				["Double Dungeon"] = 3,
-				["Planet Namak"] = 1,
-				["Spirit Society"] = 6,
-				["Shibuya Station"] = 4,
-				["Underground Church"] = 5,
-				["Sand Village"] = 2
-			}
-		}
-	},
-	["Macros"] = {
-		["Macro Retry Limit"] = 0,
-		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true
-	},
-	["Failsafe"] = {
-		["Auto Rejoin"] = true
-	},
-	["Secure"] = {
-		["Walk Around"] = true
-	},
-	["Winter Portal Joiner"] = {
-		["Buy if out of Portal"] = true,
-		["Auto Join"] = true,
-		["Tier Cap"] = 10,
-		["Portal Reward Picker"] = {
-			["Enable"] = true,
-			["Prioritize"] = {
-				["Shibuya Aftermath"] = 2,
-				["Spider Forest"] = 1,
-				["Planet Namak"] = 3
-			}
-		},
-		["Auto Next"] = true,
-		["Auto Delete Spider Forest Portal"] = true
-	},
-	["Claimer"] = {
-		["Auto Claim Milestone"] = true,
-		["Auto Claim Quest"] = true,
-		["Auto Claim Achievement"] = true,
-		["Auto Claim Daily Reward"] = true,
-		["Auto Claim Collection"] = true,
-		["Auto Claim Enemy Index"] = true,
-		["Auto Claim Collection Milestone"] = true
-	},
-	["Gameplay"] = {
-		["Auto Sell"] = {
-			["Enable"] = true,
-			["Wave"] = 22
-		},
-		["Saber Event"] = {
-			["Servant"] = "Berserker"
-		},
-		["Auto Vote Start"] = true,
-		["Auto Skip Wave"] = true,
-		["Auto Use Ability"] = true,
-		["Auto Sell Farm"] = {
-			["Enable"] = true,
-			["Wave"] = 20
-		}
-	},
-	["Stage Joiner"] = {
-		["Auto Join"] = true,
-		["Act"] = "Infinite",
-		["Stage"] = "Planet Namak"
 	}
 }
 getgenv().Key = "k7d27caec454d21cbd95104d"
