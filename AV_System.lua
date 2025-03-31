39c73ac965af7233ea9c303422b47bd1215776d8
Sendo_cmd <champandbank1@gmail.com>
2025-04-01 04:01:09 +0700

    a

--- AV_System.lua
+++ AV_System.lua
@@ -319,7 +319,7 @@ task.spawn(function()
             print("Im here")
             while true do
                 if AllPlayerInGame() then
-                    Next_(120)
+                    Next_(140)
                     if AllPlayerInGame() then 
                         local Portal = PortalSettings(GetItem(Settings_["ID"]))
                         if Portal then
