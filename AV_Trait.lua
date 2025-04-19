a4c4e2f34def89366359f97465447c0da34c9128
Sendo_cmd <champandbank1@gmail.com>
2025-04-20 00:08:56 +0700

    a

--- AV_Trait.lua
+++ AV_Trait.lua
@@ -1,87 +1,133 @@
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-getgenv().RedeemAllCode = true
-getgenv().EquipMacroUnit = true
 getgenv().Config = {
+	["AutoSave"] = true,
 	["Stage Joiner"] = {
 		["Auto Join"] = true,
 		["Act"] = "Act1",
 		["Stage"] = "Planet Namak"
 	},
 	["Macros"] = {
-		["No Ignore Sell Timing"] = true,
-		["Ignore Macro Timing"] = true
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true
+	},
+	["Love Portal Joiner"] = {
+		["Tier Cap"] = 10,
+		["Portal Reward Picker"] = {
+			["Prioritize"] = {
+				["Double Dungeon"] = 3,
+				["Planet Namak"] = 1,
+				["Spirit Society"] = 6,
+				["Shibuya Station"] = 4,
+				["Underground Church"] = 5,
+				["Sand Village"] = 2
+			}
+		}
+	},
+	["Winter Portal Joiner"] = {
+		["Tier Cap"] = 10,
+		["Portal Reward Picker"] = {
+			["Prioritize"] = {
+				["Shibuya Aftermath"] = 2,
+				["Spider Forest"] = 1,
+				["Planet Namak"] = 3
+			}
+		}
 	},
 	["Claimer"] = {
-		["Auto Claim Battle Pass"] = true,
 		["Auto Claim Quest"] = true,
-		["Auto Claim Achievement"] = true,
 		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Battle Pass"] = true,
+		["Auto Claim Achievement"] = true,
 		["Auto Claim Collection"] = true,
 		["Auto Claim Enemy Index"] = true,
 		["Auto Claim Milestone"] = true
 	},
-	["Match Finished"] = {
-		["Auto Replay"] = true
+	["Gameplay"] = {
+		["Double Dungeon"] = {
+			["Auto Statue"] = true,
+			["Statue Unit"] = "Rom and Ran (Fanatic)",
+			["Leave Extra Money"] = 5000,
+			["Upgrade Amount"] = 0
+		},
+		["Auto Use Ability"] = true,
+		["Saber Event"] = {
+			["Servant"] = "Berserker"
+		},
+		["Auto Vote Start"] = true,
+		["Auto Skip Wave"] = true,
+		["Auto Sell Farm"] = {
+			["Wave"] = 1
+		},
+		["Shibuya Station"] = {
+			["Upgrade Amount"] = 0,
+			["Auto Mohato"] = true,
+			["Leave Extra Money"] = 5000,
+			["Mohato Unit"] = "Rom and Ran (Fanatic)"
+		}
+	},
+	["Daily Challenge Joiner"] = {
+		["Auto Join"] = true
 	},
 	["Modifier"] = {
 		["Auto Modifier"] = {
-			["Enable"] = true,
 			["Prioritize"] = {
 				["Strong"] = 0,
 				["Thrice"] = 0,
 				["Warding off Evil"] = 0,
 				["Champions"] = 1,
 				["Fast"] = 0,
-				["Revitalize"] = 0,
+				["Planning Ahead"] = 0,
 				["Immunity"] = 100,
 				["Exploding"] = 99,
 				["Dodge"] = 0,
 				["Slayer"] = 0,
 				["Fisticuffs"] = 0,
-				["Planning Ahead"] = 0,
+				["Revitalize"] = 0,
 				["Harvest"] = 0,
 				["Quake"] = 0,
 				["Range"] = 0,
-				["Lifeline"] = 0,
 				["No Trait No Problem"] = 0,
-				["Regen"] = 0,
+				["Drowsy"] = 0,
 				["King's Burden"] = 0,
+				["Regen"] = 0,
 				["Exterminator"] = 0,
 				["Damage"] = 0,
 				["Common Loot"] = 0,
 				["Cooldown"] = 0,
-				["Drowsy"] = 0,
+				["Lifeline"] = 0,
 				["Press It"] = 0,
 				["Precise Attack"] = 0,
 				["Shielded"] = 0,
 				["Uncommon Loot"] = 0,
 				["Money Surge"] = 0
 			},
+			["Enable"] = true,
 			["Amount"] = {
 				["Strong"] = 0,
 				["Thrice"] = 0,
 				["Warding off Evil"] = 0,
 				["Champions"] = 0,
 				["Fast"] = 0,
-				["Revitalize"] = 0,
+				["Planning Ahead"] = 0,
 				["Fisticuffs"] = 0,
 				["Exploding"] = 0,
 				["Dodge"] = 0,
 				["Uncommon Loot"] = 0,
 				["Immunity"] = 0,
-				["Planning Ahead"] = 0,
+				["Revitalize"] = 0,
 				["Harvest"] = 0,
 				["Precise Attack"] = 0,
 				["Range"] = 0,
-				["Lifeline"] = 0,
 				["No Trait No Problem"] = 0,
+				["Drowsy"] = 0,
 				["Cooldown"] = 0,
 				["Exterminator"] = 0,
-				["King's Burden"] = 0,
+				["Regen"] = 0,
 				["Damage"] = 0,
 				["Common Loot"] = 0,
-				["Regen"] = 0,
-				["Drowsy"] = 0,
+				["King's Burden"] = 0,
+				["Lifeline"] = 0,
 				["Press It"] = 0,
 				["Quake"] = 0,
 				["Shielded"] = 0,
@@ -90,54 +136,28 @@ getgenv().Config = {
 			}
 		}
 	},
-	["AutoSave"] = true,
-	["Daily Challenge Joiner"] = {
-		["Auto Join"] = true,
-	},
-	["Regular Challenge Joiner"] = {
-		["Auto Join"] = true,
-		["Reward"] = "Trait Reroll Challenge"
-	},
-	["Weekly Challenge Joiner"] = {
-		["Auto Join"] = true,
-	},
-	["Failsafe"] = {
-		["Auto Rejoin"] = true,
-		["Teleport Lobby if Player"] = false
-	},
-	["Performance Failsafe"] = {
-		["Ping Freeze"] = true,
-		["Teleport Lobby FPS below"] = {
-			["Enable"] = true,
-			["FPS"] = 4
-		}
-	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["Secure"] = {
-		["Random Offset"] = true,
-		["Walk Around"] = true
-	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
 		["Enable"] = true,
 		["Upgrade Method"] = "Hotbar left to right (until Max)",
 		["Prefer Position"] = {
-			["Golden Castle"] = "Middle",
 			["Double Dungeon"] = "Middle",
-			["Planet Namak"] = "Middle",
-			["Tracks at the Edge of the World"] = "Middle",
-			["Shibuya Station"] = "Middle",
+			["Cavern"] = "Middle",
 			["Sand Village"] = "Middle",
 			["Shibuya Aftermath"] = "Middle",
+			["Mountain Shrine (Natural)"] = "Middle",
 			["Kuinshi Palace"] = "Middle",
+			["Land of the Gods"] = "Middle",
+			["Golden Castle"] = "Middle",
+			["Spirit Society"] = "Middle",
+			["Tracks at the Edge of the World"] = "Middle",
+			["Martial Island"] = "Middle",
+			["Shibuya Station"] = "Middle",
+			["Planet Namak"] = "Middle",
+			["Ant Island"] = "Middle",
 			["Underground Church"] = "Middle",
 			["Spider Forest"] = "Middle",
-			["Spirit Society"] = "Middle"
+			["Blood-Red Chamber"] = "Middle"
 		},
 		["Upgrade Cap"] = {
 			["1"] = 0,
@@ -147,41 +167,47 @@ getgenv().Config = {
 			["4"] = 0,
 			["6"] = -1
 		},
-		["Focus on Farm"] = true,
 		["Middle Position"] = {
 			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781",
 			["Golden Castle"] = "-100.51405334472656, -0.16030120849609375, -210.62820434570312",
 			["Spider Forest"] = "-324.47222900390625, 1644.5369873046875, -319.5542297363281",
 			["Kuinshi Palace"] = "395.2952880859375, 268.38262939453125, 114.03340148925781"
+		},
+		["Focus on Farm"] = true
+	},
+	["Match Finished"] = {
+		["Replay Amount"] = 0,
+		["Auto Replay"] = true
+	},
+	["Performance Failsafe"] = {
+		["Ping Freeze"] = true,
+		["Teleport Lobby FPS below"] = {
+			["Enable"] = true,
+			["FPS"] = 4
 		}
-	},	
-	["Gameplay"] = {
-		["Double Dungeon"] = {
-			["Auto Statue"] = true,
-			["Statue Unit"] = "Rom and Ran (Fanatic)",
-			["Upgrade Amount"] = 0
-		},
-		["Auto Sell"] = {
-			["Enable"] = false,
-			["Wave"] = 1
-		},
-		["Auto Restart"] = {
-			["Enable"] = false,
-			["Wave"] = 1
-		},
-		["Auto Vote Start"] = true,
-		["Auto Use Ability"] = true,
-		["Auto Skip Wave"] = true,
-		["Auto Sell Farm"] = {
-			["Enable"] = false,
-			["Wave"] = 1
-		},
-		["Shibuya Station"] = {
-			["Mohato Unit"] = "Rom and Ran (Fanatic)",
-			["Auto Mohato"] = true,
-			["Upgrade Amount"] = 0
-		}
+	},
+	["Failsafe"] = {
+		["Auto Rejoin"] = true,
+		["Disable Auto Teleport AFK Chamber"] = true
+	},
+	["Weekly Challenge Joiner"] = {
+		["Auto Join"] = true
+	},
+	["Secure"] = {
+		["Random Offset"] = true,
+		["Walk Around"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["Regular Challenge Joiner"] = {
+		["Reward"] = "Trait Reroll Challenge",
+		["Auto Join"] = true,
+		["Teleport Lobby new Challenge"] = true
 	}
 }
 getgenv().Key = "ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH"
