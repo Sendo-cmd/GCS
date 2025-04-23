48823948d92b45546962be085e941fd94b4b7d07
Sendo_cmd <champandbank1@gmail.com>
2025-04-23 16:01:33 +0700

    a

--- AV_Trait.lua
+++ AV_Trait.lua
@@ -2,7 +2,6 @@ game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 getgenv().Config = {
 	["AutoSave"] = true,
 	["Stage Joiner"] = {
-		["Auto Join"] = true,
 		["Act"] = "Act1",
 		["Stage"] = "Planet Namak"
 	},
@@ -35,10 +34,10 @@ getgenv().Config = {
 		}
 	},
 	["Claimer"] = {
-		["Auto Claim Collection Milestone"] = true,
 		["Auto Claim Quest"] = true,
-		["Auto Claim Achievement"] = true,
+		["Auto Claim Collection Milestone"] = true,
 		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Achievement"] = true,
 		["Auto Claim Collection"] = true,
 		["Auto Claim Enemy Index"] = true,
 		["Auto Claim Milestone"] = true
@@ -50,7 +49,12 @@ getgenv().Config = {
 			["Leave Extra Money"] = 5000,
 			["Upgrade Amount"] = 0
 		},
-		["Auto Use Ability"] = true,
+		["Shibuya Station"] = {
+			["Auto Mohato"] = true,
+			["Mohato Unit"] = "Rom and Ran (Fanatic)",
+			["Leave Extra Money"] = 0,
+			["Upgrade Amount"] = 1
+		},
 		["The System"] = {
 			["Auto Shadow"] = {
 				["Enable"] = true,
@@ -60,39 +64,22 @@ getgenv().Config = {
 		["Saber Event"] = {
 			["Servant"] = "Berserker"
 		},
+		["Auto Vote Start"] = true,
+		["Auto Skip Wave"] = true,
+		["Auto Use Ability"] = true,
 		["Auto Sell Farm"] = {
 			["Enable"] = true,
 			["Wave"] = 15
-		},
-		["Auto Vote Start"] = true,
-		["Auto Skip Wave"] = true,
-		["Shibuya Station"] = {
-			["Mohato Unit"] = "Rom and Ran (Fanatic)",
-			["Auto Mohato"] = true,
-			["Leave Extra Money"] = 2000,
-			["Upgrade Amount"] = 1
 		}
 	},
 	["Daily Challenge Joiner"] = {
 		["Auto Join"] = true
 	},
 	["Regular Challenge Joiner"] = {
-		["Reward"] = "Trait Reroll Challenge",
+		["Teleport Lobby new Challenge"] = true,
 		["Auto Join"] = true,
-		["Teleport Lobby new Challenge"] = true
+		["Reward"] = "Trait Reroll Challenge"
 	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["Match Finished"] = {
-		["Auto Next"] = true,
-		["Replay Amount"] = 0,
-		["Auto Replay"] = true
-	},
-	["AutoExecute"] = true,
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
 		["Place Cap"] = {
@@ -116,13 +103,13 @@ getgenv().Config = {
 			["Golden Castle"] = "Middle",
 			["Spirit Society"] = "Middle",
 			["Ant Island"] = "Middle",
-			["Martial Island"] = "Middle",
 			["Shibuya Station"] = "Middle",
+			["Blood-Red Chamber"] = "Middle",
 			["Cavern"] = "Middle",
 			["Tracks at the Edge of the World"] = "Middle",
 			["Underground Church"] = "Middle",
 			["Spider Forest"] = "Middle",
-			["Blood-Red Chamber"] = "Middle"
+			["Martial Island"] = "Middle"
 		},
 		["Upgrade Cap"] = {
 			["1"] = 0,
@@ -130,27 +117,33 @@ getgenv().Config = {
 			["2"] = 0,
 			["5"] = 0,
 			["4"] = 0,
-			["6"] = 0,
+			["6"] = 0
 		},
 		["Focus on Farm"] = true,
 		["Middle Position"] = {
-			["Shibuya Aftermath"] = "-94.77557373046875, 316.1539306640625, -34.29759216308594",
+			["Shibuya Aftermath"] = "223.9261474609375, 514.7516479492188, 60.541473388671875",
+			["Double Dungeon"] = "-268.2764892578125, 0.10069097578525543, -119.23352813720703",
 			["Planet Namak"] = "540.7099609375, 2.062572717666626, -365.3252258300781",
+			["Kuinshi Palace"] = "395.2952880859375, 268.38262939453125, 114.03340148925781",
 			["Golden Castle"] = "-100.51405334472656, -0.16030120849609375, -210.62820434570312",
 			["Spider Forest"] = "-324.47222900390625, 1644.5369873046875, -319.5542297363281",
-			["Kuinshi Palace"] = "395.2952880859375, 268.38262939453125, 114.03340148925781"
+			["Land of the Gods"] = "-158.61436462402344, 1.2214683294296265, 120.46851348876953"
 		}
 	},
+	["Match Finished"] = {
+		["Auto Next"] = true,
+		["Replay Amount"] = 0,
+		["Return Lobby Failsafe"] = true,
+		["Auto Replay"] = true
+	},
+	["AutoExecute"] = true,
 	["Failsafe"] = {
 		["Disable Auto Teleport AFK Chamber"] = true,
 		["Auto Rejoin"] = true
 	},
-	["Weekly Challenge Joiner"] = {
-		["Auto Join"] = true
-	},
 	["Secure"] = {
-		["Random Offset"] = true,
-		["Walk Around"] = true
+		["Walk Around"] = true,
+		["Random Offset"] = true
 	},
 	["Modifier"] = {
 		["Auto Modifier"] = {
@@ -162,28 +155,28 @@ getgenv().Config = {
 				["Champions"] = 1,
 				["Fast"] = 0,
 				["Revitalize"] = 0,
-				["Immunity"] = 100,
+				["Fisticuffs"] = 0,
 				["Exploding"] = 99,
 				["Dodge"] = 0,
-				["Slayer"] = 0,
-				["Fisticuffs"] = 0,
+				["Uncommon Loot"] = 0,
+				["Immunity"] = 100,
 				["Planning Ahead"] = 0,
 				["Harvest"] = 0,
-				["Quake"] = 0,
+				["Precise Attack"] = 0,
 				["Range"] = 0,
 				["Drowsy"] = 0,
 				["Lifeline"] = 0,
-				["Regen"] = 0,
-				["King's Burden"] = 0,
+				["Cooldown"] = 0,
 				["Exterminator"] = 0,
+				["Regen"] = 0,
 				["Damage"] = 0,
 				["Common Loot"] = 0,
-				["Cooldown"] = 0,
+				["King's Burden"] = 0,
 				["No Trait No Problem"] = 0,
 				["Press It"] = 0,
-				["Precise Attack"] = 0,
+				["Quake"] = 0,
 				["Shielded"] = 0,
-				["Uncommon Loot"] = 0,
+				["Slayer"] = 0,
 				["Money Surge"] = 0
 			},
 			["Amount"] = {
@@ -193,31 +186,40 @@ getgenv().Config = {
 				["Champions"] = 0,
 				["Fast"] = 0,
 				["Revitalize"] = 0,
-				["Fisticuffs"] = 0,
+				["Immunity"] = 0,
 				["Exploding"] = 0,
 				["Dodge"] = 0,
-				["Uncommon Loot"] = 0,
-				["Immunity"] = 0,
+				["Slayer"] = 0,
+				["Fisticuffs"] = 0,
 				["Planning Ahead"] = 0,
 				["Harvest"] = 0,
-				["Precise Attack"] = 0,
+				["Quake"] = 0,
 				["Range"] = 0,
 				["Drowsy"] = 0,
 				["Lifeline"] = 0,
-				["Cooldown"] = 0,
-				["Exterminator"] = 0,
+				["Regen"] = 0,
 				["King's Burden"] = 0,
+				["Exterminator"] = 0,
 				["Damage"] = 0,
 				["Common Loot"] = 0,
-				["Regen"] = 0,
+				["Cooldown"] = 0,
 				["No Trait No Problem"] = 0,
 				["Press It"] = 0,
-				["Quake"] = 0,
+				["Precise Attack"] = 0,
 				["Shielded"] = 0,
-				["Slayer"] = 0,
+				["Uncommon Loot"] = 0,
 				["Money Surge"] = 0
 			}
 		}
+	},
+	["Weekly Challenge Joiner"] = {
+		["Auto Join"] = true
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
 	}
 }
 getgenv().Key = "ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH"
