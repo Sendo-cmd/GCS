2264fc49d61861261201b93f4a11f41a3082642a
Sendo_cmd <champandbank1@gmail.com>
2025-05-10 08:40:55 +0700

    a

--- AV_CIDP.lua
+++ AV_CIDP.lua
@@ -9,16 +9,19 @@ getgenv().Config = {
 				["Raid Joiner"] = 0,
 				["Dungeon Joiner"] = 0,
 				["Weekly Challenge Joiner"] = 0,
-				["Worldline Joiner"] = 0,
 				["Stage Joiner"] = 0,
-				["Regular Challenge Joiner"] = 0,
-				["Boss Bounties Joiner"] = 0,
+				["Worldline Joiner"] = 0,
 				["Winter Portal Joiner"] = 0,
+				["Boss Bounties Joiner"] = 0,
+				["Regular Challenge Joiner"] = 0,
 				["Daily Challenge Joiner"] = 0,
 				["Rift Joiner"] = 0
 			}
 		},
 		["No Ignore Sell Timing"] = true
+	},
+	["Stat Reroller"] = {
+		["Stat Potential"] = 100
 	},
 	["Winter Portal Joiner"] = {
 		["Tier Cap"] = 10,
@@ -31,15 +34,18 @@ getgenv().Config = {
 		}
 	},
 	["Claimer"] = {
-		["Auto Claim Milestone"] = true,
 		["Auto Claim Quest"] = true,
-		["Auto Claim Achievement"] = true,
 		["Auto Claim Daily Reward"] = true,
+		["Auto Claim Milestone"] = true,
+		["Auto Claim Achievement"] = true,
 		["Auto Claim Collection"] = true,
 		["Auto Claim Enemy Index"] = true,
 		["Auto Claim Collection Milestone"] = true
 	},
 	["Gameplay"] = {
+		["Auto Restart"] = {
+			["Wave"] = 1
+		},
 		["Double Dungeon"] = {
 			["Upgrade Amount"] = 0,
 			["Leave Extra Money"] = 5000
@@ -52,12 +58,9 @@ getgenv().Config = {
 				["Wave"] = 1
 			}
 		},
-		["Shibuya Station"] = {
-			["Upgrade Amount"] = 0,
-			["Leave Extra Money"] = 5000
-		},
 		["Ruined City"] = {
-			["Use Mount to Travel"] = true
+			["Use Mount to Travel"] = true,
+			["Active Tower"] = true
 		},
 		["The System"] = {
 			["Auto Shadow"] = {
@@ -71,101 +74,17 @@ getgenv().Config = {
 		["Auto Use Ability"] = true,
 		["Auto Vote Start"] = true,
 		["Auto Skip Wave"] = true,
-		["Auto Restart"] = {
-			["Wave"] = 1
-		},
 		["Auto Sell Farm"] = {
 			["Wave"] = 1
+		},
+		["Shibuya Station"] = {
+			["Upgrade Amount"] = 0,
+			["Leave Extra Money"] = 5000
 		}
 	},
 	["Misc"] = {
 		["Redeem Code"] = true,
 		["Max Camera Zoom"] = 40
-	},
-	["Modifier"] = {
-		["Auto Modifier"] = {
-			["Prioritize"] = {
-				["Strong"] = 3,
-				["Thrice"] = 4,
-				["Warding off Evil"] = 24,
-				["Champions"] = 12,
-				["Fast"] = 1,
-				["Revitalize"] = 6,
-				["Immunity"] = 11,
-				["Exploding"] = 2,
-				["Dodge"] = 10,
-				["Slayer"] = 16,
-				["Fisticuffs"] = 25,
-				["Planning Ahead"] = 15,
-				["Harvest"] = 17,
-				["Quake"] = 9,
-				["Range"] = 18,
-				["Drowsy"] = 8,
-				["Lifeline"] = 29,
-				["King's Burden"] = 27,
-				["Regen"] = 7,
-				["Exterminator"] = 28,
-				["Damage"] = 20,
-				["Common Loot"] = 21,
-				["Cooldown"] = 19,
-				["No Trait No Problem"] = 23,
-				["Press It"] = 14,
-				["Precise Attack"] = 13,
-				["Shielded"] = 5,
-				["Uncommon Loot"] = 22,
-				["Money Surge"] = 26
-			},
-			["Amount"] = {
-				["Strong"] = 0,
-				["Thrice"] = 0,
-				["Warding off Evil"] = 0,
-				["Champions"] = 0,
-				["Fast"] = 0,
-				["Revitalize"] = 0,
-				["Immunity"] = 0,
-				["Exploding"] = 0,
-				["Dodge"] = 0,
-				["Slayer"] = 0,
-				["Fisticuffs"] = 0,
-				["Planning Ahead"] = 0,
-				["Harvest"] = 0,
-				["Quake"] = 0,
-				["Range"] = 0,
-				["Drowsy"] = 0,
-				["Lifeline"] = 0,
-				["King's Burden"] = 0,
-				["Regen"] = 0,
-				["Exterminator"] = 0,
-				["Damage"] = 0,
-				["Common Loot"] = 0,
-				["Cooldown"] = 0,
-				["No Trait No Problem"] = 0,
-				["Press It"] = 0,
-				["Precise Attack"] = 0,
-				["Shielded"] = 0,
-				["Uncommon Loot"] = 0,
-				["Money Surge"] = 0
-			}
-		}
-	},
-	["Performance"] = {
-		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true,
-		["Delete Entities"] = true
-	},
-	["Match Finished"] = {
-		["Replay Amount"] = 0,
-		["Auto Replay"] = true
-	},
-	["Performance Failsafe"] = {
-		["Teleport Lobby FPS below"] = {
-			["FPS"] = 5
-		}
-	},
-	["Failsafe"] = {
-		["Auto Rejoin"] = true,
-		["Disable Auto Teleport AFK Chamber"] = true
 	},
 	["Auto Play"] = {
 		["Auto Upgrade"] = true,
@@ -181,7 +100,7 @@ getgenv().Config = {
 		["Upgrade Method"] = "Hotbar left to right (until Max)",
 		["Prefer Position"] = {
 			["Double Dungeon"] = "Middle",
-			["Cavern"] = "Middle",
+			["Planet Namak"] = "Middle",
 			["Sand Village"] = "Middle",
 			["Shibuya Aftermath"] = "Middle",
 			["Mountain Shrine (Natural)"] = "Middle",
@@ -190,14 +109,14 @@ getgenv().Config = {
 			["Golden Castle"] = "Middle",
 			["Spirit Society"] = "Middle",
 			["Ant Island"] = "Middle",
-			["Martial Island"] = "Middle",
+			["Shibuya Station"] = "Middle",
 			["Ruined City"] = "Middle",
 			["Tracks at the Edge of the World"] = "Middle",
-			["Planet Namak"] = "Middle",
-			["Shibuya Station"] = "Middle",
+			["Cavern"] = "Middle",
+			["Blood-Red Chamber"] = "Middle",
 			["Underground Church"] = "Middle",
 			["Spider Forest"] = "Middle",
-			["Blood-Red Chamber"] = "Middle"
+			["Martial Island"] = "Middle"
 		},
 		["Upgrade Cap"] = {
 			["1"] = 0,
@@ -213,7 +132,7 @@ getgenv().Config = {
 		["Focus on Farm"] = true,
 		["Place Gap"] = {
 			["Double Dungeon"] = 2,
-			["Cavern"] = 2,
+			["Planet Namak"] = 2,
 			["Sand Village"] = 2,
 			["Shibuya Aftermath"] = 2,
 			["Mountain Shrine (Natural)"] = 2,
@@ -222,23 +141,20 @@ getgenv().Config = {
 			["Golden Castle"] = 2,
 			["Spirit Society"] = 2,
 			["Ant Island"] = 2,
-			["Martial Island"] = 2,
+			["Shibuya Station"] = 2,
 			["Ruined City"] = 2,
 			["Tracks at the Edge of the World"] = 2,
-			["Planet Namak"] = 2,
-			["Shibuya Station"] = 2,
+			["Cavern"] = 2,
+			["Blood-Red Chamber"] = 2,
 			["Underground Church"] = 2,
 			["Spider Forest"] = 2,
-			["Blood-Red Chamber"] = 2
+			["Martial Island"] = 2
 		}
 	},
-	["Unit Feeder"] = {
-		["Feed Level"] = 60
+	["Match Finished"] = {
+		["Replay Amount"] = 0,
+		["Auto Replay"] = true
 	},
-	["Secure"] = {
-		["Random Offset"] = true
-	},
-	["Joiner Cooldown"] = 0,
 	["Crafter"] = {
 		["Essence Stone"] = {
 			["Pink Essence Stone"] = true,
@@ -255,8 +171,93 @@ getgenv().Config = {
 			["Purple Essence Stone"] = 50
 		}
 	},
-	["Stat Reroller"] = {
-		["Stat Potential"] = 100
+	["Failsafe"] = {
+		["Disable Auto Teleport AFK Chamber"] = true,
+		["Auto Rejoin"] = true
+	},
+	["Performance Failsafe"] = {
+		["Teleport Lobby FPS below"] = {
+			["FPS"] = 5
+		}
+	},
+	["Secure"] = {
+		["Random Offset"] = true
+	},
+	["Joiner Cooldown"] = 0,
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["Modifier"] = {
+		["Auto Modifier"] = {
+			["Prioritize"] = {
+				["Strong"] = 3,
+				["Thrice"] = 4,
+				["Warding off Evil"] = 24,
+				["Champions"] = 12,
+				["Fast"] = 1,
+				["Planning Ahead"] = 15,
+				["Immunity"] = 11,
+				["Exploding"] = 2,
+				["Dodge"] = 10,
+				["Slayer"] = 16,
+				["Fisticuffs"] = 25,
+				["Revitalize"] = 6,
+				["Harvest"] = 17,
+				["Quake"] = 9,
+				["Range"] = 18,
+				["Lifeline"] = 29,
+				["No Trait No Problem"] = 23,
+				["Regen"] = 7,
+				["King's Burden"] = 27,
+				["Exterminator"] = 28,
+				["Damage"] = 20,
+				["Common Loot"] = 21,
+				["Cooldown"] = 19,
+				["Drowsy"] = 8,
+				["Press It"] = 14,
+				["Precise Attack"] = 13,
+				["Shielded"] = 5,
+				["Uncommon Loot"] = 22,
+				["Money Surge"] = 26
+			},
+			["Amount"] = {
+				["Strong"] = 0,
+				["Thrice"] = 0,
+				["Warding off Evil"] = 0,
+				["Champions"] = 0,
+				["Fast"] = 0,
+				["Planning Ahead"] = 0,
+				["Immunity"] = 0,
+				["Exploding"] = 0,
+				["Dodge"] = 0,
+				["Slayer"] = 0,
+				["Fisticuffs"] = 0,
+				["Revitalize"] = 0,
+				["Harvest"] = 0,
+				["Quake"] = 0,
+				["Range"] = 0,
+				["Lifeline"] = 0,
+				["No Trait No Problem"] = 0,
+				["Regen"] = 0,
+				["King's Burden"] = 0,
+				["Exterminator"] = 0,
+				["Damage"] = 0,
+				["Common Loot"] = 0,
+				["Cooldown"] = 0,
+				["Drowsy"] = 0,
+				["Press It"] = 0,
+				["Precise Attack"] = 0,
+				["Shielded"] = 0,
+				["Uncommon Loot"] = 0,
+				["Money Surge"] = 0
+			}
+		}
+	},
+	["Unit Feeder"] = {
+		["Feed Level"] = 60
 	}
 }
 getgenv().Key = "ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH"
