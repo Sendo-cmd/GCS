33b158d0f832056928fa075ba60ccb21342e09c6
Sendo_cmd <champandbank1@gmail.com>
2025-06-06 20:03:53 +0700

    a

--- AV_KeyGilgamesh.lua
+++ AV_KeyGilgamesh.lua
@@ -147,5 +147,5 @@ getgenv().Config = {
 		}
 	}
 }
-getgenv().Key = "ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH" -- key main[ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH , PkaqtHwDxdKOeNHlsbAJXeFpOCYXbCQB]
+getgenv().Key = "PkaqtHwDxdKOeNHlsbAJXeFpOCYXbCQB" -- key main[ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH , PkaqtHwDxdKOeNHlsbAJXeFpOCYXbCQB]
 repeat wait(4)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(6)until Joebiden
