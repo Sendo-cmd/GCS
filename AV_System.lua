983e606b934d20610903d89840918a98b5d1716e
Sendo_cmd <champandbank1@gmail.com>
2025-06-15 17:44:41 +0700

    a

--- AV_System.lua
+++ AV_System.lua
@@ -1035,17 +1035,7 @@ task.spawn(function()
                     local GameData = GameHandler.GameData
 
                     if GameData.StageType ~= "Rift" and GameData.StageType ~= "Rifts" then
-                        local Http = game:GetService("HttpService") 
-                        local TPS = game:GetService("TeleportService") 
-                        local Api = "https://games.roblox.com/v1/games/" 
-                        local _place = 16146832113 local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100" 
-                        function ListServers(cursor) 
-                            local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or "")) 
-                            return Http:JSONDecode(Raw) 
-                        end 
-                        local Server, Next; 
-                        repeat local Servers = ListServers(Next) Server = Servers.data[1] Next = Servers.nextPageCursor 
-                        until Server TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
+                        game:GetService("ReplicatedStorage").Networking.TeleportEvent:FireServer("Lobby")
                     end
                 end
                 task.wait(30)
