eb2409b506fcaa475f24d9ed0fa8b04517518899
Sendo_cmd <champandbank1@gmail.com>
2025-07-16 23:36:31 +0700

    a

--- ASTDX_Cha.lua
+++ ASTDX_Cha.lua
@@ -14,6 +14,7 @@ getgenv().Config = {
 	},
 	["Challenge Joiner"] = {
 		["Auto Join"] = true,
+		["Expert Mode"] = true,
 		["Modifier"] = {
 			["Flying Enemies"] = true,
 			["Juggernaut Enemies"] = true,
