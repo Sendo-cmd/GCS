de4e2c1d914c09934eca81f6c865ecb00c062883
Sendo_cmd <champandbank1@gmail.com>
2025-07-31 23:26:47 +0700

    a

--- AV_System2.lua
+++ AV_System2.lua
@@ -515,17 +515,6 @@ local function Register_Room(myproduct,player)
                 ["FriendsOnly"] = false
             }
             end,
-            ["44013587-aa9e-4ca9-8c5a-8503fb61779b"] = function()
-                Settings["Select Mode"] = "Story",
-                Settings["Story Settings"] = {
-                ["Difficulty"] = "Normal",
-                ["Act"] = "infinite",
-                ["StageType"] = "Story",
-                ["Stage"] = "Planet Namak",
-                ["FriendsOnly"] = false
-            },
-            Settings["Auto Join Challenge"] = true
-            end,
         }
         if Changes[myproduct] then
             Changes[myproduct]()
