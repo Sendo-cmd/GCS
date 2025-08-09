2723959327d5993a9516b53ed38320738380dc88
Sendo_cmd <champandbank1@gmail.com>
2025-08-09 21:22:33 +0700

    a

--- AV_System2.lua
+++ AV_System2.lua
@@ -722,7 +722,7 @@ local function Register_Room(myproduct,player)
                 SendUUID(player,"Normal",value["GUID"])
             end
         end)
-        local WaitTime = 180
+        local WaitTime = 300
         if Settings["Auto Join Rift"] and workspace:GetAttribute("IsRiftOpen") then
             while true do
                 if AllPlayerInGame(player) then
