f348be34c4e1d324a10bc84add58ed781a9caf5f
Sendo_cmd <champandbank1@gmail.com>
2025-08-14 18:14:34 +0700

    a

--- AV_System.lua
+++ AV_System.lua
@@ -978,8 +978,8 @@ local function Auto_Config()
                    Post(PathWay .. "finished", CreateBody())
                 end
             elseif Product["condition"]["type"] == "round" then
-                local AlreadyFarm = MatchProdunct("win")
-                if AlreadyFarm > Goal then
+                print(tonumber(OrderData["progress_value"]) , tonumber(OrderData["target_value"]))
+                if tonumber(OrderData["progress_value"]) >= tonumber(OrderData["target_value"]) then
                     if _G.Leave_Party then _G.Leave_Party() end
                      Post(PathWay .. "finished", CreateBody())
                 end
