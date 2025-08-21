ac272fead239d25360cc14a243fcfe91d7184836
Sendo_cmd <champandbank1@gmail.com>
2025-08-21 12:29:52 +0700

    a

--- AV_System2.lua
+++ AV_System2.lua
@@ -1162,7 +1162,7 @@ task.spawn(function()
                                 else
                                     UpdateCache(Username,{["current_play"] = ""}) 
                                 end
-                                Waiting_Time = Waiting_Time + 75
+                                Waiting_Time = Waiting_Time + 60
                             end
                             Last_Message = message["message-id"]
                             task.wait(3)
