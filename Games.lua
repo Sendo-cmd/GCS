644b0535526e53cc1053bd534f37b09a7db8bfc3
Sendo_cmd <champandbank1@gmail.com>
2025-08-24 14:36:49 +0700

    a

--- Games.lua
+++ Games.lua
@@ -2835,6 +2835,12 @@ Accounts = {
         "System",
         "System2",
     },
+    ["RochetteM7364"] = {
+        "Log",
+        "Reroll",
+        "System",
+        "System2",
+    },
 }
 repeat task.wait(5) until game:IsLoaded(5)
 
