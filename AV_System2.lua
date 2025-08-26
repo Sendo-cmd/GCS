154d2d0b1426a33c23fda318ef8296df0e169551
Sendo_cmd <champandbank1@gmail.com>
2025-08-27 01:26:10 +0700

    a

--- AV_System2.lua
+++ AV_System2.lua
@@ -1177,7 +1177,7 @@ task.spawn(function()
                 local Attempt = 0
                 local Last_Message = nil
                 local Current_Party = GetParty()
-                local Waiting_Time = os.time() + 240
+                local Waiting_Time = os.time() + 180
                 -- Auto Accept Party
                 task.spawn(function()
                     while task.wait(1) do
@@ -1214,7 +1214,7 @@ task.spawn(function()
                                 else
                                     UpdateCache(Username,{["current_play"] = ""}) 
                                 end
-                                Waiting_Time = Waiting_Time + 75
+                                Waiting_Time = Waiting_Time + 130
                             end
                             Last_Message = message["message-id"]
                             task.wait(3)
