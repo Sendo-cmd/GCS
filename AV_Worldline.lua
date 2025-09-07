5c8f6474c785930fdb28ad907b566d7aaa3c97f8
Sendo_cmd <champandbank1@gmail.com>
2025-09-08 06:26:55 +0700

    a

--- AV_Worldline.lua
+++ AV_Worldline.lua
@@ -856,6 +856,6 @@ getgenv().Config = {
 			["Extra Time"] = 60
 		}
 	}
-}}
+}
 getgenv().Key = "k7d27caec454d21cbd95104d" -- key main[ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH , k7d27caec454d21cbd95104d]
 repeat wait(1)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(2)until Joebiden
