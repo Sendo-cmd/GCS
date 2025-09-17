ccce5cdb712f220355f5259b294d07db85229704
Sendo_cmd <champandbank1@gmail.com>
2025-09-17 20:40:23 +0700

    a

--- AV_Base.lua
+++ AV_Base.lua
@@ -924,4 +924,4 @@ getgenv().Config = {
 	}
 }
 getgenv().Key = "k7d27caec454d21cbd95104d" 
-repeat wait(4)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(2)until Joebiden
+repeat wait(1)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(1)until Joebiden
