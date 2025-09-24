eef023e5d8dbedcf30851ef41c40432af8e51cef
Sendo_cmd <champandbank1@gmail.com>
2025-09-25 00:55:58 +0700

    a

--- AV_Base.lua
+++ AV_Base.lua
@@ -805,7 +805,7 @@ getgenv().Config = {
 				["Tyrant Destroyer"] = 86,
 				["Range"] = 95,
 				["Planning Ahead"] = 0,
-				["Tyrant Arrives"] = 88,
+				["Tyrant Arrives"] = 87,
 				["High Class"] = 0,
 				["Immunity"] = 91,
 				["Harvest"] = 99,
@@ -838,7 +838,7 @@ getgenv().Config = {
 				["Revitalize"] = 89,
 				["Wild Card"] = 33,
 				["Slayer"] = 92,
-				["Sphere Finder"] = 87
+				["Sphere Finder"] = 86
 			},
 			["Amount"] = {
 				["Champions"] = 1,
