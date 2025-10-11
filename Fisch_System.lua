73a208a88574fb75d3a2e342064321ff4d77efc3
Sendo_cmd <champandbank1@gmail.com>
2025-10-11 08:00:55 +0700

    a

--- Fisch_System.lua
+++ Fisch_System.lua
@@ -1,1 +1,30 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rxito/ZenithHub/refs/heads/main/Loader"))()
+repeat  task.wait() until game:IsLoaded()
+game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+local VIM = game:GetService('VirtualInputManager')
+local tloading = tick() + 5
+local loading
+repeat task.wait()
+    loading = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("loading")
+until tick() >= tloading or loading
+
+while loading and loading.Enabled do task.wait()
+    if loading.Enabled and loading.loading.skip.Visible then
+        local skip = loading.loading.skip
+        skip.AnchorPoint = Vector2.new(.5,.5)
+        skip.Position = UDim2.fromScale(.5,.5)
+        skip.Size = UDim2.fromScale(9999,9999)
+        print("Skip")
+    end
+    local Vector = {workspace.CurrentCamera.ViewportSize.X / 2, workspace.CurrentCamera.ViewportSize.Y / 2}
+    VIM:SendMouseButtonEvent(Vector[1],Vector[2], 0, true, game, 1)
+    VIM:SendMouseButtonEvent(Vector[1],Vector[2], 0, false, game, 1)
+end 
+
+task.spawn(function()
+    while true do
+         local Vector = {workspace.CurrentCamera.ViewportSize.X / 2, workspace.CurrentCamera.ViewportSize.Y / 2}
+    VIM:SendMouseButtonEvent(Vector[1],Vector[2], 0, true, game, 1)
+    VIM:SendMouseButtonEvent(Vector[1],Vector[2], 0, false, game, 1)
+        task.wait(10)
+    end
+end)
