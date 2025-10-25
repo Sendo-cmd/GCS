7b66287f2ea14e2771c18bd06b39c60456ec450d
Sendo_cmd <champandbank1@gmail.com>
2025-10-26 05:42:26 +0700

    a

--- ALS_Base.lua
+++ ALS_Base.lua
@@ -114,5 +114,5 @@ getgenv().Config = {
 		["No Ignore Sell Timing"] = true
 	}
 }
-getgenv().Key = "k7d27caec454d21cbd95104d" 
+getgenv().Key = "ElIsfyTzBhioWvdmAjNPUaUaCHtnQvwH" 
 repeat wait(1)spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(1)until Joebiden
