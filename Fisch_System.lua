33f21cb909603d368cec03ef4f9a81901e39f229
Sendo_cmd <champandbank1@gmail.com>
2025-10-31 06:20:01 +0700

    a

--- Fisch_System.lua
+++ Fisch_System.lua
@@ -300,7 +300,7 @@ local function Auto_Config()
                     end
                 elseif Product["condition"]["type"] == "hour" then
                     print(tonumber(OrderData["progress_value"]) , Goal ,OrderData["target_value"]/60/60,OrderData["target_value"])
-                    if tonumber(OrderData["progress_value"]) >= (tonumber(OrderData["target_value"])/60/60) then
+                    if tonumber(OrderData["progress_value"]) == (tonumber(OrderData["target_value"])/60/60) then
                         print("Finish")
                         Post(PathWay .. "finished", CreateBody())
                     end
@@ -326,14 +326,7 @@ local function Auto_Config()
     end)
 end
 
-
-
-
-
-
 Auto_Config()
-
-
 
 local library = require(ReplicatedStorage.shared.modules.library);
 for i,v in pairs(library.rods) do
