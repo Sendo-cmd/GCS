91eb3f9541c69502c91da89839048868a781ae0d
Sendo_cmd <champandbank1@gmail.com>
2025-11-03 00:13:44 +0700

    a

--- AV_System.lua
+++ AV_System.lua
@@ -1453,6 +1453,12 @@ local function Auto_Config()
                     if _G.Leave_Party then _G.Leave_Party() end
                      Post(PathWay .. "finished", CreateBody())
                 end
+            elseif Product["condition"]["type"] == "level" then
+                print(tonumber(OrderData["progress_value"]) , Goal)
+                if tonumber(OrderData["progress_value"]) >= (tonumber(OrderData["target_value"])) then
+                    if _G.Leave_Party then _G.Leave_Party() end
+                     Post(PathWay .. "finished", CreateBody())
+                end
             end
         end
             -- Post(PathWay .. "finished", {
