affb19c73ecac0635bd85ff837c440514b6e2abb
Sendo_cmd <champandbank1@gmail.com>
2025-11-11 20:13:23 +0700

    a

--- Fisch_System.lua
+++ Fisch_System.lua
@@ -314,12 +314,12 @@ local function Auto_Config()
                 elseif Product["condition"]["type"] == "hour" then
                     print(tonumber(OrderData["progress_value"]) , Goal ,OrderData["target_value"]/60/60,OrderData["target_value"])
                     if tonumber(OrderData["progress_value"]) >= (tonumber(OrderData["target_value"])/60/60) then
-                        print("Finish")
                         Post(PathWay .. "finished", CreateBody())
                     end
                 elseif Product["condition"]["type"] == "level" then
                     print(tonumber(OrderData["progress_value"]) , Goal)
                     if tonumber(OrderData["progress_value"]) >= (tonumber(OrderData["target_value"])) then
+                        print("Finish")
                         Post(PathWay .. "finished", CreateBody())
                     end
                 elseif Product["condition"]["type"] == "round" then
