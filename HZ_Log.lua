672e916ddff73c5ddb764c0bf16114884ddc97c0
Sendo_cmd <champandbank1@gmail.com>
2025-12-01 22:26:09 +0700

    a

--- HZ_Log.lua
+++ HZ_Log.lua
@@ -181,13 +181,12 @@ task.spawn(function ()
     local gamestart = workspace:GetAttribute("gamestart") or 0
     repeat task.wait() until workspace:GetAttribute("gameend")
     local timetaken = (workspace:GetAttribute("gameend") or workspace:GetServerTimeNow()) - gamestart
-    local MasteryLevel = Tool_:GetAttribute("MasteryLevel") or 1
     local function Send(val,data)
         local StageInfo = {
             ["win"] = val,
             ["map"] = {
                 ["name"] = tostring(workspace:GetAttribute("Mapname")),
-                ["chapter"] = tostring(MasteryLevel),
+                ["chapter"] = "0",
                 ["wave"] = "0",
                 ["mode"] = tostring(workspace:GetAttribute("Mode")),
                 ["difficulty"] = tostring(workspace:GetAttribute("Gamemode")),
