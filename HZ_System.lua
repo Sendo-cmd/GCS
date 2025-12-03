4fae93e3d7a9209fc75a640aac7a2d2786815459
Sendo_cmd <champandbank1@gmail.com>
2025-12-03 16:07:22 +0700

    a

--- HZ_System.lua
+++ HZ_System.lua
@@ -664,7 +664,7 @@ local function Teleport_()
 end
 
 if getrenv()["shared"]["loaded"] then
-    setfpscap(20) task.wait(1)
+    task.wait(1)
     task.delay(60,function()
       Teleport_()
     end)
