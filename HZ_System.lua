97c4545934fca861dd788d67803ae6c215ed82d1
Sendo_cmd <champandbank1@gmail.com>
2025-12-07 20:58:05 +0700

    a

--- HZ_System.lua
+++ HZ_System.lua
@@ -1118,7 +1118,7 @@ else
         end)
     elseif GameType[LevelObject.Value.Name] == "Payload" then
         print("H2")
-        setfpscap(11)
+        setfpscap(13)
         local PauseToTakeItem = false
         local Pickup = false
         local BreakToKill = tick() + 10
