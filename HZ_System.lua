317515dd8cf71ef1afa0aeddfc3385ecc978ed35
Sendo_cmd <champandbank1@gmail.com>
2025-12-10 06:09:54 +0700

    a

--- HZ_System.lua
+++ HZ_System.lua
@@ -1261,15 +1261,15 @@ else
                                     return false;
                                 end
                                 Pickup = true
-                                Character.HumanoidRootPart.CFrame = v.PickupHitbox.CFrame task.wait(.4)
+                                Character.HumanoidRootPart.CFrame = v.PickupHitbox.CFrame task.wait(.32)
                                 Pickup = false
-                                lasttake = tick() + .67
+                                lasttake = tick() + .7
                                 if Health then
                                     task.wait(.75)
                                 end
                             end
                         end
-                        lasttake = tick() + .67
+                        lasttake = tick() + .7
                     end
                     
                 end)
