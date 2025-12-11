1eb6642bd1a0dad16c1586f47be9c6a0c2cd77ca
Sendo_cmd <champandbank1@gmail.com>
2025-12-11 10:50:54 +0700

    a

--- HZ_System.lua
+++ HZ_System.lua
@@ -1342,7 +1342,7 @@ else
                                 
                                 lasttake = tick() + .75
                                 if Health then
-                                    task.wait(.77)
+                                    task.wait(.76)
                                 else
                                     Pickup = false
                                 end
