dca2ab3f5f3ae8e6ec667f252f1bd72544ae03cb
Sendo_cmd <champandbank1@gmail.com>
2025-12-22 17:16:14 +0700

    ฟ

--- HZ_System.lua
+++ HZ_System.lua
@@ -1040,7 +1040,7 @@ else
                     end
                 end
                 if not Workspace:GetAttribute("DATA_ID") then
-                    Workspace:SetAttribute(Tool:GetAttribute("DATA_ID"),.27)
+                    Workspace:SetAttribute(Tool:GetAttribute("DATA_ID"),.25)
                 end
                
             end
