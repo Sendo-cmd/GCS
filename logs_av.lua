72903186ff45d8780df3881c3cb86aa6c73c6c16
Sendo_cmd <champandbank1@gmail.com>
2026-01-21 18:04:37 +0700

    a

--- logs_av.lua
+++ logs_av.lua
@@ -34,7 +34,7 @@ local Modules = ReplicatedStorage:WaitForChild("Modules")
 task.wait(1.5)
 local IsTimeChamber = game.PlaceId == 18219125606
 
-local url = "https://api.championshop.date/logs"
+-- local url = "https://api.championshop.date/logs"
 print(game.PlaceId)
 
 local function convertToField(index,value)
